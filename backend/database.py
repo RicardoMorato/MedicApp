6649f1ba857badcf70eee91a44fc6dc1115d519f
h3nrey <pedro.novaes.education@gmail.com>
2025-02-05 08:56:47 -0300

    FEAT: setup basic database configuration

---
+++ backend/database.py
@@ -0,0 +1,23 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, Session
+from fastapi import Depends
+
+DATABASE_URL = "postgresql://admin:Medicap4563@localhost:5432/medicapp"
+
+# Create engine
+engine = create_engine(DATABASE_URL)
+
+# Session factory
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Base class for models
+Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
