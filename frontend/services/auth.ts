42f80503be58739939534f961c982ec07cd87ba9
pedorooo <phln@cin.ufpe.br>
2025-03-16 19:41:15 -0300

    FIX: lidando com erros no login

--- frontend/services/auth.ts
+++ frontend/services/auth.ts
@@ -1,77 +1,86 @@
 import api from "./api";
 import { Alert } from "react-native";
-import  AsyncStorage  from "@react-native-async-storage/async-storage"
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 type FormData = {
-  Nome: string
-  Email: string
-  Senha: string
-}
+  Nome: string;
+  Email: string;
+  Senha: string;
+};
 
 type FormDataLogin = {
-  Email: string
-  Senha: string
-}
+  Email: string;
+  Senha: string;
+};
 
 export const onSubmit = async (
-  data: FormData, 
+  data: FormData,
   navigation: any,
   setLoading: (loading: boolean) => void
 ) => {
   try {
-    setLoading(true)
-    const response = await api.post('/users/signup', {
+    setLoading(true);
+    const response = await api.post("/users/signup", {
       name: data.Nome,
       email: data.Email,
-      password: data.Senha
-    })
+      password: data.Senha,
+    });
 
-    if (response.status === 200) {
-      Alert.alert('Cadastro realizado com sucesso', '\nFaça login para continuar.')
-      
+    if (response.status === 201) {
+      Alert.alert(
+        "Cadastro realizado com sucesso",
+        "\nFaça login para continuar."
+      );
+
       setTimeout(() => {
-        setLoading(false)
-        navigation.replace('Signin') 
-      }, 1500)
+        setLoading(false);
+        navigation.replace("MainHome");
+      }, 1500);
     }
-    
   } catch (error: any) {
-    Alert.alert('Erro', error.response?.data?.detail || 'Erro ao realizar cadastro. Tente novamente.');
+    Alert.alert(
+      "Erro",
+      error.response?.data?.detail ||
+        "Erro ao realizar cadastro. Tente novamente."
+    );
   }
-}
+};
 
 export const login = async (
-  data: FormDataLogin, 
-  navigation: any, 
+  data: FormDataLogin,
+  navigation: any,
   setLoading: (loading: boolean) => void
 ) => {
   try {
-    setLoading(true)
-    const response = await api.post('/users/login', {
+    setLoading(true);
+    const response = await api.post("/users/login", {
       email: data.Email,
-      password: data.Senha
-    })
+      password: data.Senha,
+    });
 
-    if (response.status === 200) {
-      AsyncStorage.setItem('userToken', response.data.token)
-      AsyncStorage.setItem('isLoggedIn', JSON.stringify(true)) 
+    if (response.status === 201) {
+      AsyncStorage.setItem("userToken", response.data.token);
+      AsyncStorage.setItem("isLoggedIn", JSON.stringify(true));
       setTimeout(() => {
-        setLoading(false)
-        Alert.alert('Sucesso', 'Login realizado com sucesso!')
-        navigation.replace('MainHome') 
-      }, 1000)// Usando replace em vez de navigate 
-      // por que ao tentar voltar quando estiver no home, 
+        setLoading(false);
+        Alert.alert("Sucesso", "Login realizado com sucesso!");
+        navigation.replace("MainHome");
+      }, 1000); // Usando replace em vez de navigate
+      // por que ao tentar voltar quando estiver no home,
       // nao voltar para a tela de login, apenas se sair da conta
-    } 
+    }
   } catch (error: any) {
-    Alert.alert('Erro', error.response?.data?.detail || 'Erro ao realizar login. Tente novamente.');
+    Alert.alert(
+      "Erro",
+      error.response?.data?.detail || "Erro ao realizar login. Tente novamente."
+    );
+    setLoading(false);
   }
-}
+};
 
 export const logOut = async (navigation: any) => {
-  await AsyncStorage.removeItem('userToken')
-  await AsyncStorage.removeItem('isLoggedIn')
-  Alert.alert('Você saiu!', 'Faça login novamente para acessar sua conta.')
-  navigation.replace('SignOut') 
-
-}
+  await AsyncStorage.removeItem("userToken");
+  await AsyncStorage.removeItem("isLoggedIn");
+  Alert.alert("Você saiu!", "Faça login novamente para acessar sua conta.");
+  navigation.replace("SignOut");
+};
