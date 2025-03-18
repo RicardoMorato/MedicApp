680ec693077b0d321b254e36ee2fd83e2abd2d51
Weldon <weldonbarros179@gmail.com>
2025-03-18 08:04:50 -0300

    Tela de adicionar medicamentos

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -2,6 +2,7 @@ import {createNativeStackNavigator} from "@react-navigation/native-stack"
 
 import Signin from "@/pages/Signin" 
 import Signup from "@/pages/Signup"
+import AddMedicine from "@/pages/AddMedicine"
 const Stack = createNativeStackNavigator()
 
 export default function Routes(){
@@ -16,6 +17,12 @@ export default function Routes(){
             <Stack.Screen
             name="Signup"
             component={Signup}
+            options={{headerShown: false}}
+            />
+
+            <Stack.Screen
+            name="AddMedicine"
+            component={AddMedicine}
             options={{headerShown: false}}
             />
             
