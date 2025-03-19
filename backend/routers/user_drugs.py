ebc389872927c50c47644b2576bffb5cc829c9d6
joão <jantdantas@gmail.com>
2025-03-19 19:46:44 -0300

    fix: router HTTP status

--- backend/routers/user_drugs.py
+++ backend/routers/user_drugs.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 from controllers import user_drugs as controller  
 from database import get_db
@@ -7,6 +7,6 @@ from dependencies.auth_dependency import get_current_user
 
 router = APIRouter()
 
-@router.post("/users/drugs")
+@router.post("/users/{user_id}/drugs/", status_code=status.HTTP_201_CREATED)
 def add_drug(drug: DrugCreate, db: Session = Depends(get_db), current_user=Depends(get_current_user)):
     return controller.add_drug_to_user(db, current_user, drug)
