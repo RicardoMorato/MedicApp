33f6ed954847abeaee3a08814595d9a16793944f
eduardomatos7 <eduardo.matoss@ufpe.br>
2025-03-21 18:13:37 -0300

    feat: add MedicationDetails page - in construction

---
+++ frontend/pages/MedicationDetails/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+function MedicationDetails() {
+  return (
+    <View style={{
+        flex: 1, 
+        alignItems: 'center', 
+        justifyContent: 'center'
+    }}><Text style={{fontSize: 30}}>Tela em construçãooo</Text></View>
+)
+}
+
+export default MedicationDetails
