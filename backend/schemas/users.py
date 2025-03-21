9e6c6ca6a952ceeb9427b7e0e5f98bf99f0d5f2e
João Antônio <120070923+joaoadsobral@users.noreply.github.com>
2025-03-21 20:19:40 -0300

    fix: validation error in user register in backend (#36)

--- backend/schemas/users.py
+++ backend/schemas/users.py
@@ -1,12 +1,27 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, validator
+from fastapi import HTTPException, status
+import re
 
 class UserBase(BaseModel):
     name: str
-    email: str
+    email: EmailStr
 
 class UserCreate(UserBase):
     password: str
 
+    @validator('password')
+    def validate_password(cls, password: str):
+        password_regex = r'^(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*()_+={}:;,.<>?/-]).{8,}$'
+
+        if not re.match(password_regex, password):
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail=(
+                    "A senha deve conter pelo menos 8 caracteres, uma letra maiúscula, uma letra minúscula e um caractere especial."
+                )
+            )
+
+        return password
 class UserLogin(BaseModel):
-    email: str
+    email: EmailStr
     password: str
