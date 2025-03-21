04a28d1b235c5e016ddae7b6958988a02ed04af1
h3nrey <pedro.novaes.education@gmail.com>
2025-03-21 19:48:45 -0300

    feat: adicionando os elects

--- frontend/global/colors.ts
+++ frontend/global/colors.ts
@@ -1,11 +1,13 @@
 const colors = {
-    primary: '#419DFF',
-    secondary: '#0057B3',
-    background: '#fff',
-    textPrimary: '#000',
-    textSecondary: '#333',
-    inputLoginBackground: '#D9D9D9',
-    error: 'red',
+  primary: "#419DFF",
+  secondary: "#0057B3",
+  background: "#fff",
+  textPrimary: "#000",
+  textSecondary: "#333",
+  inputLoginBackground: "#D9D9D9",
+  error: "red",
+  lightGray: "#2F2F2F",
+  darkBg: "#2F2F2F",
 };
 
 export default colors;
