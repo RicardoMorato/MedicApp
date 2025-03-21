a14e5bb687dac05dadfe29f8d78779d9e8511277
eduardomatos7 <eduardo.matoss@ufpe.br>
2025-03-21 18:10:50 -0300

    feat: add Item component -- logic for card expansion and styling

---
+++ frontend/components/Item/Item.tsx
@@ -0,0 +1,54 @@
+import { Text, View, TouchableOpacity, Animated } from 'react-native';
+import React, { useState } from 'react';
+import { Medication } from '../../components/Medicaments/Medication';
+import { styles } from '../styles/style';
+import { useNavigation } from '@react-navigation/native';
+
+export interface ItemProps {
+  item: Medication;
+}
+
+export const Item: React.FC<ItemProps> = ({ item }) => {
+  const [expanded, setExpanded] = useState(false);
+  const [animation] = useState(new Animated.Value(0));
+  const navigation = useNavigation<any>();
+
+  const toggleExpand = () => {
+    setExpanded(!expanded);
+    Animated.timing(animation, {
+      toValue: expanded ? 0 : 1,
+      duration: 300,
+      useNativeDriver: false,
+    }).start();
+  };
+
+  const lengthDescription = item.description.length;
+  const height = animation.interpolate({
+    inputRange: [0, 1],
+    outputRange: lengthDescription > 43 ? [0, 100] : [0, 80],
+  });
+
+  return (
+    <TouchableOpacity onPress={toggleExpand}>
+      <View style={styles.card}>
+        <View style={styles.TitleDosageSectionRow}>
+          <Text style={styles.itemName}>{item.name}</Text>
+          <View style={styles.dosageBg}>
+            <Text style={styles.itemDosage}>{item.dosage}</Text>
+          </View>
+        </View>
+        <Text style={styles.itemDosage}>{item.category}</Text>
+        <Animated.View style={{ height, overflow: 'hidden' }}>
+          <View style={styles.dividerCard} />
+          <Text style={styles.itemDescription}>{item.description}</Text>
+          <TouchableOpacity
+            style={styles.linkToDetails}
+            onPress={() => navigation.navigate('MedicationDetails')}
+          >
+            <Text style={styles.details}>Ver detalhes</Text>
+          </TouchableOpacity>
+        </Animated.View>
+      </View>
+    </TouchableOpacity>
+  );
+};
