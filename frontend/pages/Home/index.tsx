4dece6f74e55d46b8a40d2848959bd887e7e65e2
pedorooo <phln@cin.ufpe.br>
2025-03-22 00:13:00 -0300

    FEAT: adicionando a tela de interacao medicamentosa

--- frontend/pages/Home/index.tsx
+++ frontend/pages/Home/index.tsx
@@ -2,13 +2,9 @@ import React from "react";
 import { View, Text, TouchableOpacity } from "react-native";
 import styles from "./style";
 import { logOut } from "@/services/auth";
-<<<<<<< HEAD
 import { useNavigation } from 'expo-router';
 import colors from '@/global/colors';
 
-=======
-import { useNavigation } from "expo-router";
->>>>>>> 138b450 (FEAT: adicionando a tela de interacao medicamentosa)
 
 function Home() {
   const navigation = useNavigation<any>();
@@ -44,16 +40,14 @@ function Home() {
           </TouchableOpacity>
         </View>
       </View>
-<<<<<<< HEAD
-    </View>
     <View style={styles.horizontalLine} />
      <View style={{top: 20, display: 'flex', gap: 20, width: '100%', alignItems: 'center' }}>
         <Text onPress={() => navigation.navigate('AddMedicine')} style={{color: colors.secondary, textDecorationLine: 'underline', fontFamily: 'Poppins_300Light'}}>Adicionar medicamento</Text>
         <Text onPress={() => navigation.navigate('MedicationList')} style={{color: colors.secondary, textDecorationLine: 'underline', fontFamily: 'Poppins_300Light'}}>ir para lista de medicamentos</Text>
       </View>
-=======
+
       <View style={styles.horizontalLine} />
->>>>>>> 138b450 (FEAT: adicionando a tela de interacao medicamentosa)
+
     </>
   );
 }
