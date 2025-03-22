138b450b0836f23476d68cef0e8d180f25ea1f4e
pedorooo <phln@cin.ufpe.br>
2025-03-22 00:13:00 -0300

    FEAT: adicionando a tela de interacao medicamentosa

--- frontend/pages/Home/style.ts
+++ frontend/pages/Home/style.ts
@@ -1,31 +1,32 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-    container: {
-        padding: 20,
-        backgroundColor: '#f8f8f8',
-    },
-    notiIcon: {
-        paddingBottom: 10,
-        alignItems: 'flex-end',
-    },
-    home: {
-        display: 'flex',
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        padding: 10,
-    },
-    logo: {
-        fontSize: 23,
-        fontWeight: 'bold',
-        color: '#333',
-    },
-    horizontalLine: {
-        borderBottomColor: 'black',
-        borderBottomWidth: 1,
-        marginVertical: 10,
-    }
-})
+  container: {
+    padding: 20,
+    backgroundColor: "#f8f8f8",
+  },
+  notiIcon: {
+    paddingBottom: 10,
+    alignItems: "flex-end",
+  },
+  header: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    padding: 10,
+  },
+  home: {},
+  logo: {
+    fontSize: 23,
+    fontWeight: "bold",
+    color: "#333",
+  },
+  horizontalLine: {
+    borderBottomColor: "black",
+    borderBottomWidth: 1,
+    marginVertical: 10,
+  },
+});
 
-export default styles
+export default styles;
--- frontend/services/drugs.service.ts
+++ frontend/services/drugs.service.ts
@@ -1,4 +1,4 @@
-import { drugsInteractions } from "@/data";
+import { drugs, drugsInteractions } from "@/data";
 import { Drug } from "@/type";
 
 export async function checkDrugInteraction(drugs: Drug[]) {
@@ -9,10 +9,17 @@ export async function checkDrugInteraction(drugs: Drug[]) {
 function hasDrugInteraction(drugA: any, drugB: any) {
   console.log(drugA);
   console.log(drugB);
+  const idA = drugs.filter((drug) => drug.name === drugA)[0].id;
+  const idB = drugs.filter((drug) => drug.name === drugB)[0].id;
+
+  console.log(`id a: ${idA}, drugA: ${drugA}`);
+  console.log(`id b: ${idB}, drugb: ${drugB}`);
   for (const drug of drugsInteractions) {
+    console.log("drug Interaction");
+    console.log(drug);
     if (
-      (drug.drugId == drugA && drug.interactedId == drugB) ||
-      (drug.drugId == drugB && drug.interactedId == drugA)
+      (drug.drugId == idA && drug.interactedId == idB) ||
+      (drug.drugId == idB && drug.interactedId == idA)
     )
       return true;
   }
