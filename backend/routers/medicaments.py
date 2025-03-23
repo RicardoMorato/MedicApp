2dbdade39a6babd6f2cbb5cf8c072a038fb9161e
joão <jantdantas@gmail.com>
2025-03-23 18:30:34 -0300

    feat: addind first logic to the check drug interaction

--- backend/routers/medicaments.py
+++ backend/routers/medicaments.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 from database import get_db
-from schemas.medicaments import MedicamentResponse
+from schemas.medicaments import MedicamentResponse, MedicamentCall
 from controllers.medicaments import add_medicament as controller  
 
 router = APIRouter()
@@ -9,3 +9,7 @@ router = APIRouter()
 @router.post("/drugs/add", status_code=status.HTTP_201_CREATED)
 def add_drug(drug: MedicamentResponse, db: Session = Depends(get_db)):
     return controller(db, drug)
+
+@router.get("drug/check drug interaction", status_code=status.HTTP_200_OK)
+def check_drugs(drug: MedicamentCall, db: Session = Depends(get_db)):
+    return
