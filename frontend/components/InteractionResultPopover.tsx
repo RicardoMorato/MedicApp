3b0bd15e7783a2743510545c9f396d1bb6666a58
pedorooo <phln@cin.ufpe.br>
2025-03-23 13:31:34 -0300

    FIX: corrigindo inconsitencias com o celular

--- frontend/components/InteractionResultPopover.tsx
+++ frontend/components/InteractionResultPopover.tsx
@@ -1,5 +1,6 @@
 import { Colors } from "@/constants/Colors";
 import { View, Text, Image, TouchableOpacity } from "react-native";
+import { Portal, Provider } from "react-native-paper";
 export default function InteractionResultPopover({
   result,
   drugA,
@@ -22,88 +23,87 @@ export default function InteractionResultPopover({
     closeCallback();
   }
   return (
-    <TouchableOpacity
-      onPress={(e: any) => closeModal(e, 1)}
-      style={{
-        padding: 50,
-        paddingHorizontal: 8,
-        gap: 12,
-        backgroundColor: Colors.dark.background,
-        height: "100%",
-        borderRadius: 12,
-        alignItems: "center",
-        position: "fixed",
-        zIndex: 10,
-        top: 0,
-        justifyContent: "center",
-        cursor: "pointer",
-        width: "100%",
-        left: "50%",
-        transform: "translateX(-50%)",
-      }}
-    >
+    <Portal>
       <TouchableOpacity
-        onPress={(e: any) => closeModal(e, 0)}
+        onPress={(e: any) => closeModal(e, 1)}
         style={{
-          padding: 50,
-          paddingHorizontal: 8,
+          paddingVertical: 50,
           gap: 12,
-          backgroundColor: Colors.dark.lightBg,
+          backgroundColor: Colors.dark.background,
+          height: "100%",
           borderRadius: 12,
           alignItems: "center",
-          left: "50%",
-          transform: "translateX(-50%)",
+          position: "fixed",
+          zIndex: 10,
+          top: 0,
+          justifyContent: "center",
+          cursor: "pointer",
+          width: "100%",
         }}
       >
-        <Text
+        <TouchableOpacity
+          onPress={(e: any) => closeModal(e, 0)}
           style={{
-            color: result ? Colors.light.atention : Colors.light.green,
-            fontSize: 60,
-            fontFamily: "Poppins_300Light",
-            borderRadius: 4,
-            fontWeight: 600,
-            textAlign: "center",
+            padding: 50,
+            paddingHorizontal: 12,
+            gap: 12,
+            backgroundColor: Colors.dark.lightBg,
+            borderRadius: 12,
+            alignItems: "center",
+            width: "100%",
           }}
         >
-          {!result ? "Tudo certo" : "Atenção!"}
-        </Text>
-        <Image source={icon} style={{ width: 100, height: 100 }} />
+          <Text
+            style={{
+              color: result ? Colors.light.atention : Colors.light.green,
+              fontSize: 60,
+              fontFamily: "Poppins_300Light",
+              borderRadius: 4,
+              fontWeight: 600,
+              textAlign: "center",
+              fontStyle: "italic",
+            }}
+          >
+            {!result ? "Tudo certo" : "Atenção!"}
+          </Text>
+          <Image source={icon} style={{ width: 100, height: 100 }} />
 
-        <View>
-          {result ? (
-            <Text
-              style={{
-                color: Colors.light.whiteText,
-                fontSize: 20,
-                fontFamily: "Poppins_300Light",
-                textAlign: "center",
-                wordWrap: "break-all",
-              }}
-            >
-              Os medicamentos{" "}
-              <Text style={{ color: Colors.dark.warning }}>{drugA} </Text> e{" "}
-              <Text style={{ color: Colors.dark.warning }}>{drugB}</Text>.
-              possuem interação severa. Consulte seu médico antes de usá-los
-              juntos.
-            </Text>
-          ) : (
-            <Text
-              style={{
-                color: Colors.light.whiteText,
-                fontSize: 20,
-                fontFamily: "Poppins_300Light",
-                textAlign: "center",
-                wordWrap: "break-all",
-              }}
-            >
-              Não foram detectadas interações entre{" "}
-              <Text style={{ color: Colors.dark.green }}>{drugA} </Text> e{" "}
-              <Text style={{ color: Colors.dark.green }}>{drugB}</Text>. Esses
-              medicamentos são compatíveis.
-            </Text>
-          )}
-        </View>
+          <View>
+            {result ? (
+              <Text
+                style={{
+                  color: Colors.light.whiteText,
+                  fontSize: 20,
+                  fontFamily: "Poppins_300Light",
+                  textAlign: "center",
+                  wordWrap: "break-all",
+                }}
+              >
+                Os medicamentos{" "}
+                <Text style={{ color: Colors.dark.warning }}>{drugA} </Text> e{" "}
+                <Text style={{ color: Colors.dark.warning }}>{drugB}</Text>.
+                possuem interação severa. Consulte seu médico antes de usá-los
+                juntos.
+              </Text>
+            ) : (
+              <Text
+                style={{
+                  color: Colors.light.whiteText,
+                  fontSize: 20,
+                  fontFamily: "Poppins_300Light",
+                  textAlign: "center",
+                  wordWrap: "break-all",
+                }}
+              >
+                Não foram detectadas interações entre{" "}
+                <Text style={{ color: Colors.dark.green }}>{drugA} </Text> e{" "}
+                <Text style={{ color: Colors.dark.green }}>{drugB}</Text>. Esses
+                medicamentos são compatíveis.
+              </Text>
+            )}
+          </View>
+        </TouchableOpacity>
       </TouchableOpacity>
-    </TouchableOpacity>
+    </Portal>
   );
 }
