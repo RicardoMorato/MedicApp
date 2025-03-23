ff65a0bd9bcd95e08fb47da25b2d4649b94a2534
joão <jantdantas@gmail.com>
2025-03-23 18:12:24 -0300

    feat: adding interaction controller

--- backend/schemas/interactions.py
+++ backend/schemas/interactions.py
@@ -1,10 +1,14 @@
 from pydantic import BaseModel
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 class InteractionsResponse(BaseModel):
 =======
 class MedicamentResponse(BaseModel):
 >>>>>>> 735361f (feat: creating schemas and routers to the interactions creating)
+=======
+class InteractionsResponse(BaseModel):
+>>>>>>> 9bc0109 (feat: adding interaction controller)
     principio_ativo1: str
     principio_ativo2: str
     gravidade_interacao: str
