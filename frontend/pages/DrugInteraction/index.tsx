40f8fba43f048cbdc74243424095afcdd7c90a39
pedorooo <phln@cin.ufpe.br>
2025-03-23 13:31:34 -0300

    FIX: corrigindo inconsitencias com o celular

--- frontend/pages/DrugInteraction/index.tsx
+++ frontend/pages/DrugInteraction/index.tsx
@@ -7,6 +7,7 @@ import styles from "./style";
 import InteractionResultPopover from "@/components/InteractionResultPopover";
 import { drugs } from "@/data";
 import { Colors } from "@/constants/Colors";
+import { Provider } from "react-native-paper";
 
 export default function DrugInteraction() {
   const navigation = useNavigation<any>();
@@ -32,73 +33,72 @@ export default function DrugInteraction() {
     setInteractionResult(result);
   }
   return (
-    <View style={styles.container}>
-      <TouchableOpacity
-        style={styles.navButton}
-        onPress={() => navigation.navigate("MainHome")}
-      >
-        <Image source={require("@/assets/icons/backArrow.png")} />
-        <Text
-          style={{
-            color: Colors.dark.text,
-            fontFamily: "Poppins_700Bold",
-            fontSize: 20,
-            fontWeight: 700,
-          }}
+    <Provider>
+      <View style={styles.container}>
+        <TouchableOpacity
+          style={styles.navButton}
+          onPress={() => navigation.navigate("MainHome")}
         >
-          Verificar Interações
-        </Text>
-      </TouchableOpacity>
-      <View style={styles.content}>
-        {interactionResult != null && drugA && drugB && (
-          <InteractionResultPopover
-            drugA={drugA}
-            drugB={drugB}
-            result={interactionResult ?? true}
-            closeCallback={() => setInteractionResult(null)}
-          />
-        )}
-        <View style={styles.interactionContainer}>
-          <Text style={styles.title}>
-            Verifique se você pode tomar dois medicamentos simultâneamente.
-          </Text>
-
-          <SelectList
-            placeholder="Selecione um medicamento"
-            searchPlaceholder="Buscar"
-            setSelected={setDrugA}
-            data={parseDrugsToSelect(drugs)}
-            save="value"
-            boxStyles={styles.dropdownWrapper}
-            inputStyles={styles.dropdownText}
-            dropdownStyles={styles.dropdown}
-          />
-
-          <Image
-            source={require("@/assets/icons/Cycle.png")}
-            style={{ zIndex: -1 }}
-          />
-
-          <SelectList
-            placeholder="Selecione um medicamento"
-            searchPlaceholder="Buscar"
-            setSelected={setDrugB}
-            data={parseDrugsToSelect(drugs)}
-            save="value"
-            boxStyles={styles.dropdownWrapper}
-            inputStyles={styles.dropdownText}
-            dropdownStyles={styles.dropdown}
-          />
-
-          <TouchableOpacity
-            onPress={checkDrugsInteraction}
-            style={styles.confirmButton}
+          <Image source={require("@/assets/icons/backArrow.png")} />
+          <Text
+            style={{
+              color: Colors.dark.text,
+              fontFamily: "Poppins_700Bold",
+              fontSize: 20,
+              fontWeight: 700,
+            }}
           >
-            <Text style={styles.confirmText}>Verificar Interação</Text>
-            <Image source={require("@/assets/icons/smallCycle.png")} />
-          </TouchableOpacity>
+            Verificar Interações
+          </Text>
+        </TouchableOpacity>
+        <View style={styles.content}>
+          {interactionResult != null && drugA && drugB && (
+            <InteractionResultPopover
+              drugA={drugA}
+              drugB={drugB}
+              result={interactionResult ?? true}
+              closeCallback={() => setInteractionResult(null)}
+            />
+          )}
+          <View style={styles.interactionContainer}>
+            <Text style={styles.title}>
+              Verifique se você pode tomar dois medicamentos simultâneamente.
+            </Text>
+
+            <SelectList
+              placeholder="Selecione um medicamento"
+              searchPlaceholder="Buscar"
+              setSelected={setDrugA}
+              data={parseDrugsToSelect(drugs)}
+              save="value"
+              boxStyles={styles.dropdownWrapper}
+              inputStyles={styles.dropdownText}
+              dropdownStyles={styles.dropdown}
+            />
+
+            <Image source={require("@/assets/icons/Cycle.png")} />
+
+            <SelectList
+              placeholder="Selecione um medicamento"
+              searchPlaceholder="Buscar"
+              setSelected={setDrugB}
+              data={parseDrugsToSelect(drugs)}
+              save="value"
+              boxStyles={styles.dropdownWrapper}
+              inputStyles={styles.dropdownText}
+              dropdownStyles={styles.dropdown}
+            />
+
+            <TouchableOpacity
+              onPress={checkDrugsInteraction}
+              style={styles.confirmButton}
+            >
+              <Text style={styles.confirmText}>Verificar Interação</Text>
+              <Image source={require("@/assets/icons/smallCycle.png")} />
+            </TouchableOpacity>
+          </View>
         </View>
       </View>
-    </View>
+    </Provider>
   );
 }
