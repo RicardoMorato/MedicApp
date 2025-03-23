87e56303a62ad339ad77e6b540c001f25cf346f3
joão <jantdantas@gmail.com>
2025-03-23 17:46:16 -0300

    feat: adding medicament controller

--- backend/routers/medicaments.py
+++ backend/routers/medicaments.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 from database import get_db
-from schemas.user_drugs import DrugCreate 
+from schemas.medicaments import MedicamentResponse
+from controllers.medicaments import add_medicament as controller  
 from dependencies.auth_dependency import get_current_user
 
 router = APIRouter(prefix="/users", tags=["Medicamentos"])
 
 @router.post("/{user_id}/drugs/", status_code=status.HTTP_201_CREATED)
-def add_drug(drug: DrugCreate, db: Session = Depends(get_db), current_user=Depends(get_current_user)):
-    return controller.add_medicament(db, current_user, drug)
+def add_drug(drug: MedicamentResponse, db: Session = Depends(get_db)):
+    return controller.add_medicament(db, drug)
