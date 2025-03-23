bcd1161ba409bcf4a2e6bf4dc3899e68b6b1a8d3
joão <jantdantas@gmail.com>
2025-03-23 17:57:40 -0300

    feat: creating controller to the medication adding

---
+++ backend/routers/medicaments.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter, Depends, status
+from sqlalchemy.orm import Session
+from database import get_db
+from schemas.medicaments import MedicamentResponse
+from controllers.medicaments import add_medicament as controller  
+from dependencies.auth_dependency import get_current_user
+
+router = APIRouter()
+
+@router.post("/drugs/add", status_code=status.HTTP_201_CREATED)
+def add_drug(drug: MedicamentResponse, db: Session = Depends(get_db)):
+    return controller(db, drug)
