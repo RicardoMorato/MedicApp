71041b293c7a835b10bb55556aca996cbd6a810c
eduardomatos7 <eduardo.matoss@ufpe.br>
2025-03-23 02:33:21 -0300

    feat: create API request to retrieve data from server

--- frontend/pages/MedicationList/index.tsx
+++ frontend/pages/MedicationList/index.tsx
@@ -1,11 +1,20 @@
 import { MedicamentsListed } from '@/components/MedicamentsListed/MedicamentsListed'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import  API_URL from "../../config/config";
 
 function MedicationList() {
-  return (
-    <MedicamentsListed/>
+  const [medications, setMedications] = useState([]);
 
-)
+  useEffect(() => {
+    axios.get(`${API_URL}drugs`)
+      .then(response => {setMedications(response.data)})
+      .catch(error => console.error('Erro ao buscar medicamentos:', error));
+  }, []);
+
+  return (
+    <MedicamentsListed medications={medications} />
+  )
 }
 
 export default MedicationList
