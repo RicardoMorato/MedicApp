5ad90c2f80d68ae45f23078138ebfba2ebf8a38b
joão <jantdantas@gmail.com>
2025-03-23 20:18:58 -0300

    fix: final errors

--- backend/routers/user_drugs.py
+++ backend/routers/user_drugs.py
@@ -5,8 +5,8 @@ from database import get_db
 from schemas.user_drugs import DrugCreate 
 from dependencies.auth_dependency import get_current_user
 
-router = APIRouter(prefix="/users")
+router = APIRouter()
 
-@router.post("/{user_id}/drugs/", status_code=status.HTTP_201_CREATED)
+@router.post("/users/{user_id}/drugs/", status_code=status.HTTP_201_CREATED)
 def add_drug(drug: DrugCreate, db: Session = Depends(get_db), current_user=Depends(get_current_user)):
     return controller.add_medicament_to_user(db, current_user, drug)
