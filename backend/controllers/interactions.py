99b2d9cb236584d5b4f414a550e9c8da99534632
joão <jantdantas@gmail.com>
2025-03-23 19:35:36 -0300

    fix: improving code logic

--- backend/controllers/interactions.py
+++ backend/controllers/interactions.py
@@ -6,17 +6,7 @@ from fastapi import HTTPException, status
 from sqlalchemy import or_
 
 def add_interaction(db: Session, interaction_data: InteractionsResponse):
-    existing_interaction = db.query(Interaction).filter(
-        or_(
-            (Interaction.principio_ativo1 == interaction_data.principio_ativo1) & 
-            (Interaction.principio_ativo2 == interaction_data.principio_ativo2),
-
-            (Interaction.principio_ativo1 == interaction_data.principio_ativo2) & 
-            (Interaction.principio_ativo2 == interaction_data.principio_ativo1)
-        )
-    ).first()
-
-    if existing_interaction:
+    if existing_interaction(db,  interaction_data.principio_ativo1,  interaction_data.principio_ativo2):
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Esta interação já está cadastrada."
@@ -37,10 +27,21 @@ def add_interaction(db: Session, interaction_data: InteractionsResponse):
 
     return {"message": "Interação cadastrada com sucesso!", "interação": new_interaction}
 
-def check_interactions(db: Session, check_data: MedicamentCall):
-    drug1 = db.query(Drug.farmaco).filter(Drug.medicamento == check_data.name_1).scalar()
-    drug2 = db.query(Drug.farmaco).filter(Drug.medicamento == check_data.name_2).scalar()
+def check_interactions(db: Session, interaction_data: MedicamentCall):
+    drug1 = db.query(Drug.farmaco).filter(Drug.medicamento == interaction_data.name_1).scalar()
+    drug2 = db.query(Drug.farmaco).filter(Drug.medicamento == interaction_data.name_2).scalar()
+
+    interaction = existing_interaction(db, drug1, drug2)
+
+    if interaction:
+        return {"message": "Interação medicamentosa existente", "interação =": interaction}
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Esta interação não existe."
+        )
     
+def existing_interaction(db: Session, drug1, drug2):
     existing_interaction = db.query(Interaction).filter(
         or_(
             (Interaction.principio_ativo1 == drug1) & 
@@ -50,11 +51,4 @@ def check_interactions(db: Session, check_data: MedicamentCall):
             (Interaction.principio_ativo2 == drug1)
         )
     ).first()
-
-    if not existing_interaction:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Esta interação não existe."
-        )
-    else:
-        return {"message": "Interação medicamentosa existente", "interação =": existing_interaction}
+    return existing_interaction
