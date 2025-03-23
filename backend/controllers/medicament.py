cf7ac64bd202e582ec83c4d4a7fc714a27381360
joão <jantdantas@gmail.com>
2025-03-23 02:25:49 -0300

    feat: adding query parameters to the lister

--- backend/controllers/medicament.py
+++ backend/controllers/medicament.py
@@ -1,16 +1,16 @@
 from sqlalchemy.orm import Session
-from models import UserDrugs
+from models import Drug
 from fastapi import HTTPException
 from schemas.medicament import MedicamentResponse
+from sqlalchemy import or_
 
-def search_medicaments(db: Session, termo: str):
-    termo = f"%{termo}%"  
-    medicamentos = db.query(UserDrugs).filter(
-        (UserDrugs.name.ilike(termo))
-    )
+def search_medicaments(db: Session, termo: str, categoria: str = None, skip: int = 0, limit: int = 10):
+    termo = f"%{termo}%"
+    query = db.query(Drug).filter(Drug.medicamento.ilike(termo))
+
+    medicamentos = query.offset(skip).limit(limit).all()
 
     if not medicamentos:
         raise HTTPException(status_code=404, detail="Nenhum medicamento encontrado.")
 
     return [MedicamentResponse.model_validate(med.__dict__) for med in medicamentos]
-
--- backend/routers/medicament.py
+++ backend/routers/medicament.py
@@ -4,12 +4,14 @@ from database import get_db
 from schemas.medicament import MedicamentResponse
 from controllers.medicament import search_medicaments
 
-router = APIRouter(prefix="/medicamentos", tags=["Medicamentos"])
+router = APIRouter(prefix="/medicamentos")
+
+from fastapi import Query
 
 @router.get("/search/", response_model=list[MedicamentResponse])
-def search_medicamentos(termo: str, db: Session = Depends(get_db)):
+def search_medicamentos(termo: str, db: Session = Depends(get_db), skip: int = Query(0, ge=0), limit: int = Query(10, le=100)):
     try:
-        medicamentos = search_medicaments(db, termo)
+        medicamentos = search_medicaments(db, termo, skip=skip, limit=limit)
         return medicamentos
     except HTTPException as e:
         raise HTTPException(status_code=e.status_code, detail=e.detail)
