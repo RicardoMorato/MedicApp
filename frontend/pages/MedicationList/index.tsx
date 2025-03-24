c3cc600478918b672e7a994f1f3a01ba26a94ca8
eduardomatos7 <eduardo.matoss@ufpe.br>
2025-03-24 03:32:38 -0300

    feat: create component e and style to splash loading before list medicines

--- frontend/pages/MedicationList/index.tsx
+++ frontend/pages/MedicationList/index.tsx
@@ -2,18 +2,24 @@ import { MedicamentsListed } from '@/components/MedicamentsListed/MedicamentsLis
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import  API_URL from "../../config/config";
+import SplashLoading from '@/components/SplashLoading'
 
 function MedicationList() {
   const [medications, setMedications] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true)
     axios.get(`${API_URL}drugs`)
-      .then(response => {setMedications(response.data)})
-      .catch(error => console.error('Erro ao buscar medicamentos:', error));
-  }, []);
+    .then(response => {
+        setMedications(response.data)
+        setLoading(false)
+    })
+    .catch(error => console.error('Erro ao buscar medicamentos:', error));
+    }, []);
 
   return (
-    <MedicamentsListed medications={medications} />
+    (loading ? <SplashLoading/> : <MedicamentsListed medications={medications} />)
   )
 }
 
