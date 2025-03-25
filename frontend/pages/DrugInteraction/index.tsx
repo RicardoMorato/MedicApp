4e748953a234a1e8377c8bdcea0eaeddb9abb861
Weldon <weldonbarros179@gmail.com>
2025-03-25 18:19:57 -0300

    Alert adicionado

--- frontend/pages/DrugInteraction/index.tsx
+++ frontend/pages/DrugInteraction/index.tsx
@@ -1,7 +1,7 @@
 import { checkDrugInteraction } from "@/services/drugs.service";
 import { useNavigation } from "expo-router";
 import React, { useEffect, useState } from "react";
-import { View, Text, Image, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, Alert } from "react-native";
 import { SelectList } from "react-native-dropdown-select-list";
 import styles from "./style";
 import InteractionResultPopover from "@/components/InteractionResultPopover";
@@ -11,11 +11,9 @@ import { Ionicons } from "@expo/vector-icons";
 
 export default function DrugInteraction() {
   const navigation = useNavigation<any>();
-  const [drugA, setDrugA] = useState<string | null>("null");
-  const [drugB, setDrugB] = useState<string | null>("null");
-  const [interactionResult, setInteractionResult] = useState<boolean | null>(
-    null
-  );
+  const [drugA, setDrugA] = useState<string | null>(null);
+  const [drugB, setDrugB] = useState<string | null>(null);
+  const [interactionResult, setInteractionResult] = useState<boolean | null>(null);
 
   function parseDrugsToSelect(drugs: any[]) {
     const parsedDrugs: { key: number; value: string }[] = [];
@@ -28,20 +26,24 @@ export default function DrugInteraction() {
   useEffect(() => {}, []);
 
   async function checkDrugsInteraction() {
-    if (!drugA || !drugB) return;
+    if (!drugA || !drugB) {
+      Alert.alert("Por favor, selecione ambos os medicamentos.");
+      return;
+    }
     const result = await checkDrugInteraction([drugA, drugB]);
     setInteractionResult(result);
   }
+
   return (
     <Provider>
       <View style={styles.container}>
-      <TouchableOpacity style={styles.backButton} onPress={() => navigation.goBack()}>
-        <Ionicons name="arrow-back" size={24} color="#000" />
-        <Text style={styles.headerText}>Verificar interações</Text>
-      </TouchableOpacity>
-      <View style={styles.headerDivider}></View>
+        <TouchableOpacity style={styles.backButton} onPress={() => navigation.goBack()}>
+          <Ionicons name="arrow-back" size={24} color="#000" />
+          <Text style={styles.headerText}>Verificar interações</Text>
+        </TouchableOpacity>
+        <View style={styles.headerDivider}></View>
         <View style={styles.content}>
-          {interactionResult != null && drugA && drugB && (
+          {interactionResult != null && drugA !== null && drugB !== null && (
             <InteractionResultPopover
               drugA={drugA}
               drugB={drugB}
@@ -53,7 +55,6 @@ export default function DrugInteraction() {
             Verifique se você pode tomar dois medicamentos <Text style={styles.highlight}>simultaneamente</Text>.
           </Text>
           <View style={styles.interactionContainer}>
-
             <SelectList
               placeholder="Selecione o primeiro medicamento"
               searchPlaceholder="Buscar"
@@ -75,15 +76,13 @@ export default function DrugInteraction() {
               inputStyles={styles.dropdownText}
               dropdownStyles={styles.dropdown}
             />
-
           </View>
-          <TouchableOpacity
-              onPress={checkDrugsInteraction}
-              style={styles.button}
-            >
+          <TouchableOpacity onPress={checkDrugsInteraction} style={styles.button}>
             <Text style={styles.buttonText}>Verificar Interação</Text>
           </TouchableOpacity>
-          <Text style={styles.disclaimer}>Este aplicativo avalia interações medicamentosas, mas não substitui a consulta médica. Sempre busque orientação de um profissional de saúde.</Text>
+          <Text style={styles.disclaimer}>
+            Este aplicativo avalia interações medicamentosas, mas não substitui a consulta médica. Sempre busque orientação de um profissional de saúde.
+          </Text>
         </View>
       </View>
     </Provider>
