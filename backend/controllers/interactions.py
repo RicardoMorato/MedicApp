d9c5e3dfdce717f29b5a1457904a149ed5bfc22b
Ricardo Morato <rmr@cin.ufpe.br>
2025-03-26 12:17:56 -0300

    feat: test and refactor interactions controller
    Signed-off-by: Ricardo Morato <rmr@cin.ufpe.br>

--- backend/controllers/interactions.py
+++ backend/controllers/interactions.py
@@ -11,17 +11,30 @@ def check_interactions(db: Session, interaction_data: InteractionCall):
     interaction = existing_interaction(db, drug1, drug2)
 
     if interaction:
-        return {"message": "Interação medicamentosa existente", "interação =": interaction}
+        return {
+            "message": "Interação medicamentosa existente",
+            "interação": interaction,
+        }
     else:
         raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Esta interação não existe."
+            status_code=status.HTTP_404_NOT_FOUND, detail="Esta interação não existe."
         )
 
+
 def existing_interaction(db: Session, drug1, drug2):
-    return db.query(Interaction).filter(
-        or_(
-            and_(Interaction.principio_ativo1 == drug1, Interaction.principio_ativo2 == drug2),
-            and_(Interaction.principio_ativo1 == drug2, Interaction.principio_ativo2 == drug1)
+    return (
+        db.query(Interaction)
+        .filter(
+            or_(
+                and_(
+                    Interaction.principio_ativo1 == drug1,
+                    Interaction.principio_ativo2 == drug2,
+                ),
+                and_(
+                    Interaction.principio_ativo1 == drug2,
+                    Interaction.principio_ativo2 == drug1,
+                ),
+            )
         )
-    ).first()
+        .first()
+    )
--- backend/routers/interactions.py
+++ backend/routers/interactions.py
@@ -6,6 +6,7 @@ from controllers.interactions import check_interactions
 
 router = APIRouter()
 
-@router.post("/interactions", status_code=status.HTTP_201_CREATED)
+
+@router.post("/interactions", status_code=status.HTTP_200_OK)
 def check_drugs(drug: InteractionCall, db: Session = Depends(get_db)):
     return check_interactions(db, drug)
