0d90f469ccddbfedd6929ef7747dbfd270763ccb
joão <jantdantas@gmail.com>
2025-03-26 02:52:13 -0300

    feat: pagination in drug list

--- backend/routers/medicament.py
+++ backend/routers/medicament.py
@@ -8,5 +8,6 @@ router = APIRouter()
 
 @router.get("/medicament/search/", status_code=status.HTTP_200_OK)
 async def search_medicamentos_route(db: Session = Depends(get_db)):
-    medicamentos = db.query(Drug).all()
+    limit: int = 10 
+    medicamentos = db.query(Drug).limit(limit).all()
     return [MedicamentResponse.from_orm(med) for med in medicamentos]
