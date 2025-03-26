e81e785f6139d94fe53b0261fc533fecafe6a739
joão <jantdantas@gmail.com>
2025-03-25 21:48:49 -0300

    fix: wrong name in post router.interactions

--- backend/routers/interactions.py
+++ backend/routers/interactions.py
@@ -6,6 +6,6 @@ from controllers.interactions import check_interactions
 
 router = APIRouter()
 
-@router.post("/interactions/check-pharm-interaction", status_code=status.HTTP_201_CREATED)
+@router.post("/interactions", status_code=status.HTTP_201_CREATED)
 def check_drugs(drug: InteractionCall, db: Session = Depends(get_db)):
     return check_interactions(db, drug)
