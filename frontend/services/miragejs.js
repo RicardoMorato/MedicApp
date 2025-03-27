295d7cb0770234d86a5305e95778d2db05a30e15
eduardomatos7 <eduardo.matoss@ufpe.br>
2025-03-27 11:22:39 -0300

    fix: fixing interception bug and removing interception from medicine list

--- frontend/services/miragejs.js
+++ frontend/services/miragejs.js
@@ -8,10 +8,7 @@ async function getId() {
 }
 
 export function initializeMirage() {
-  async function getUserId() {
-    const user_id = await utilDecodeToken()
-    return user_id
-  }
+
 createServer({
   
   routes() {
@@ -22,7 +19,7 @@ createServer({
   return request;
 };
     //permite a requisição passar pelo mirage, ou seja, ele não intercepta
-    const user_id = getUserId()
+    const user_id = getId()
     this.passthrough(`${API_URL}/users/login`)
     this.passthrough(`${API_URL}/users/signup`)
     this.passthrough(`${API_URL}/users/${user_id}/drugs/`)
