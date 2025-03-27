15146f99c9db45b3b9793ed14a67c0a44dff4845
joão <jantdantas@gmail.com>
2025-03-26 23:32:37 -0300

    feat: query parameters and pagination in backend search_medicamentos

--- backend/routers/medicament.py
+++ backend/routers/medicament.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, status, Query
 from sqlalchemy.orm import Session
 from database import get_db
 from schemas.medicament import MedicamentResponse
@@ -7,6 +7,14 @@ from models import Drug
 router = APIRouter()
 
 @router.get("/medicament/search/", status_code=status.HTTP_200_OK)
-async def search_medicamentos_route(db: Session = Depends(get_db)):
-    medicamentos = db.query(Drug).all()
+async def search_medicamentos_route(db: Session = Depends(get_db), name: str = Query(None), skip: int = Query(0, ge=0), limit: int = Query(100, ge=1, le=17000)):
+    query = db.query(Drug).order_by(Drug.medicamento)
+
+    if name:
+        query = query.filter(Drug.medicamento.ilike(f"%{name}%"))
+    else:
+        query = query.offset(skip).limit(limit)
+
+    medicamentos = query.all()
+
     return [MedicamentResponse.from_orm(med) for med in medicamentos]
