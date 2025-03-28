24d432a18e3abc20a772c3815084250868fbf97a
eduardomatos7 <eduardo.matoss@ufpe.br>
2025-03-28 00:46:06 -0300

    feat: added page 'Saiba mais'

---
+++ frontend/pages/LearnMore/index.tsx
@@ -0,0 +1,16 @@
+import { Colors } from '@/constants/Colors'
+import React from 'react'
+import { View, Text } from 'react-native'
+
+
+const index = () => {
+  return (
+    
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{fontSize: 30, color: Colors.text.warning}}>Saiba Mais</Text>
+      <Text style={{textAlign: 'center', padding: 40, fontSize: 20, fontFamily: "Poppins_300Light"}}>Aqui vão ser listadas todas as informações sobre as interações medicamentosas dos medicamentos</Text>
+    </View>
+  )
+}
+
+export default index
