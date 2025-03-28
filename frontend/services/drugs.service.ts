7a69f19b390d0552e651956139224c45445e551c
eduardomatos7 <eduardo.matoss@ufpe.br>
2025-03-28 03:51:29 -0300

    fix: fixed errors and remove consolesLogs

--- frontend/services/drugs.service.ts
+++ frontend/services/drugs.service.ts
@@ -1,5 +1,4 @@
 import { drugs, drugsInteractions } from "@/data";
-import { Drug } from "@/type";
 
 export async function checkDrugInteraction(drugs: string[]) {
   const hasInteraction = hasDrugInteraction(drugs[0], drugs[1]);
@@ -7,16 +6,10 @@ export async function checkDrugInteraction(drugs: string[]) {
 }
 
 function hasDrugInteraction(drugA: any, drugB: any) {
-  console.log(drugA);
-  console.log(drugB);
   const idA = drugs.filter((drug) => drug.name === drugA)[0].id;
   const idB = drugs.filter((drug) => drug.name === drugB)[0].id;
 
-  console.log(`id a: ${idA}, drugA: ${drugA}`);
-  console.log(`id b: ${idB}, drugb: ${drugB}`);
   for (const drug of drugsInteractions) {
-    console.log("drug Interaction");
-    console.log(drug);
     if (
       (drug.drugId == idA && drug.interactedId == idB) ||
       (drug.drugId == idB && drug.interactedId == idA)
