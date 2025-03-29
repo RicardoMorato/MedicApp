dd21c77db239a031d953248028b4c9f53963a99d
Ricardo Morato <rmr@cin.ufpe.br>
2025-03-29 20:05:24 -0300

    refactor: simplify database setup

--- backend/conftest.py
+++ backend/conftest.py
@@ -5,9 +5,9 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.pool import StaticPool
 from fastapi.testclient import TestClient
 from main import app
-from database import Base, get_db
-from alembic import command
-from alembic.config import Config
+from database import get_db
+from models import Base
+
 
 SQLITE_DATABASE_URL = "sqlite:///test_db.db"
 
@@ -23,39 +23,38 @@ engine = create_engine(
 # Create a sessionmaker to manage sessions
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# Create tables in the database
-Base.metadata.create_all(bind=engine)
-
 
 @pytest.fixture(scope="function")
 def db_session():
     """Create a new database session for each test."""
     LOGGER.info("[Setup] Creating a new database session for testing")
-    connection = engine.connect()
-    transaction = connection.begin()  # Start a transaction
-    session = TestingSessionLocal(bind=connection)
+
+    LOGGER.info("[Setup] Dropping all tables in the test database")
+    # Drop all tables in the database
+    Base.metadata.drop_all(bind=engine)
+    LOGGER.info("[Setup] Creating all tables in the test database")
+    # Create all tables in the database
+    Base.metadata.create_all(bind=engine)
+
+    # Create a new database session
     try:
-        # Drop all tables to ensure a clean state
-        LOGGER.info("[Setup] Dropping all tables...")
-        Base.metadata.drop_all(bind=engine)
+        LOGGER.info("[Setup] Creating a new database session")
+        # Create a connection and start a transaction
+        # This allows us to rollback changes after each test
+        connection = engine.connect()
+        transaction = connection.begin()  # Start a transaction
+        session = TestingSessionLocal(bind=connection)
 
-        # Create all tables before applying migrations
-        LOGGER.info("[Setup] Creating all tables...")
-        Base.metadata.create_all(bind=engine)
-
-        # Apply migrations
-        alembic_cfg = Config("alembic.ini")  # Path to Alembic configuration file
-        alembic_cfg.set_main_option("sqlalchemy.url", SQLITE_DATABASE_URL)
-        LOGGER.info("[Setup] Applying migrations...")
-        command.upgrade(alembic_cfg, "head")  # Apply all migrations
-        LOGGER.info("[Setup] Migrations applied successfully.")
+        LOGGER.info(
+            "[Setup] Database session created, tables metadata below. %(payload)s",
+            {"payload": {Base.metadata.tables.values()}},
+        )
 
         yield session
     finally:
         session.close()
         transaction.rollback()  # Rollback the transaction
         connection.close()
-        LOGGER.info("[Teardown] Database session closed and transaction rolled back.")
 
 
 @pytest.fixture(scope="function")
