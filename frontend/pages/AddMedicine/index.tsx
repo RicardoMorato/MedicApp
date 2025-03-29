71b3839269e1ede6650332ececa255ca56d92962
Weldon <weldonbarros179@gmail.com>
2025-03-29 10:24:36 -0300

    incluindo a concentração e retirando o campo de genérico

--- frontend/pages/AddMedicine/index.tsx
+++ frontend/pages/AddMedicine/index.tsx
@@ -1,101 +1,88 @@
 import React, { useState } from "react";
-import { View, Text, TextInput, TouchableOpacity, Alert, ActivityIndicator } from "react-native";
+import { View, Text, TextInput, TouchableOpacity, Alert, ActivityIndicator, KeyboardAvoidingView, Platform, Image } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { Ionicons } from "@expo/vector-icons";
 import styles from "./style";
 import { addMedicine } from "@/services/addmedicine";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { jwtDecode } from "jwt-decode";
-import { MedicineData } from "../../interfaces/MedicineData";
 import { utilDecodeToken } from "@/utils/utilDecodeToken";
+import { MedicineData } from "../../interfaces/MedicineData";
+import arrow from "../../assets/images/stash--arrows-switch-duotone.png";
 
 const AddMedicine = () => {
-  
   const navigation = useNavigation();
   const [medicineName, setMedicineName] = useState("");
   const [activeIngredient, setActiveIngredient] = useState("");
-  const [isGeneric, setIsGeneric] = useState<boolean | null>(null);
-  const [brandName, setBrandName] = useState("");
-  const [loading, setLoading] = useState(false)  
+  const [concentration, setConcentration] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const handleAddMedicine = async () => {
-    if (!medicineName || !activeIngredient || isGeneric === null || (!isGeneric && !brandName)) {
+    if (!medicineName || !activeIngredient || !concentration) {
       Alert.alert("Por favor, preencha todos os campos.");
     } else {
       setMedicineName("");
       setActiveIngredient("");
-      setIsGeneric(null);
-      setBrandName("");
+      setConcentration("");
 
       const user_id = await utilDecodeToken();
-    
+      
       const data: MedicineData = {
         name: medicineName,
         activeIngredient: activeIngredient,
-        isGeneric: isGeneric,
-        brand: isGeneric ? "" : brandName,
-      };
-      
+        concentration: concentration,
+      }      
       addMedicine(data, user_id, setLoading);
     }
   };
-  
 
   return (
-    <View style={styles.container}>
-      <TouchableOpacity style={styles.backButton} onPress={() => navigation.goBack()}>
-        <Ionicons name="arrow-back" size={24} color="#000" />
-        <Text style={styles.headerText}>Adicionar medicamento</Text>
-      </TouchableOpacity>
-      <View style={styles.headerDivider}></View>
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      keyboardVerticalOffset={Platform.OS === "ios" ? 0 : -150}
+    >
+      <View style={styles.container}>
+        <TouchableOpacity style={styles.backButton} onPress={() => navigation.goBack()}>
+          <Ionicons name="arrow-back" size={24} color="#000" />
+          <Text style={styles.headerText}>Adicionar medicamento</Text>
+        </TouchableOpacity>
+        <View style={styles.headerDivider}></View>
 
+        <Text style={styles.subtitle}>Olá (nome do usuário), aqui você pode adicionar os seus próprios medicamentos</Text>
 
-      <View style={styles.form}>
-        <TextInput
-          style={styles.input}
-          placeholder="Nome do medicamento"
-          placeholderTextColor="#AAA"
-          value={medicineName}
-          onChangeText={setMedicineName}
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Princípio ativo"
-          placeholderTextColor="#AAA"
-          value={activeIngredient}
-          onChangeText={setActiveIngredient}
-        />
-
-        <Text style={styles.label}>O medicamento é genérico?</Text>
-        <View style={styles.radioContainer}>
-          <TouchableOpacity onPress={() => setIsGeneric(true)} style={styles.radioButton}>
-            <View style={isGeneric === true ? styles.radioSelected : styles.radio} />
-            <Text style={styles.radioText}>Sim</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => setIsGeneric(false)} style={styles.radioButton}>
-            <View style={isGeneric === false ? styles.radioSelected : styles.radio} />
-            <Text style={styles.radioText}>Não</Text>
-          </TouchableOpacity>
-        </View>
-
-        {!isGeneric && isGeneric !== null && (
+        <View style={styles.form}>
           <TextInput
             style={styles.input}
-            placeholder="Digite aqui a marca..."
+            placeholder="Nome do medicamento"
             placeholderTextColor="#AAA"
-            value={brandName}
-            onChangeText={setBrandName}
+            value={medicineName}
+            onChangeText={setMedicineName}
           />
-        )}
-
-
+          <TextInput
+            style={styles.input}
+            placeholder="Princípio ativo"
+            placeholderTextColor="#AAA"
+            value={activeIngredient}
+            onChangeText={setActiveIngredient}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Concentração"
+            placeholderTextColor="#AAA"
+            value={concentration}
+            onChangeText={setConcentration}
+          />
+        </View>
+        
         <TouchableOpacity style={styles.button} onPress={handleAddMedicine}>
-          {loading 
-          ? <ActivityIndicator color="#fff"/>
-          : <Text style={styles.buttonText}>Adicionar</Text>}
+          {loading ? (
+            <ActivityIndicator color="#fff" />
+          ) : (
+            <Text style={styles.buttonText}>Adicionar medicamento</Text>
+          )}
+          <Image source={arrow} resizeMode="contain" style={{ width: 30, height: 30 }} />
         </TouchableOpacity>
       </View>
-    </View>
+    </KeyboardAvoidingView>
   );
 };
 
