23b73f4292717476982804946ae66ce6803dbe72
Ricardo Morato <rmr@cin.ufpe.br>
2025-03-29 20:05:24 -0300

    refactor: simplify database setup

--- backend/database.py
+++ backend/database.py
@@ -1,5 +1,4 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 from config import Settings
@@ -16,9 +15,6 @@ engine = create_engine(DATABASE_URL)
 
 # Session factory
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# Base class for models
-Base = declarative_base()
 
 
 def get_db():
