33ad5bb1ecbf202e65904454b8cc1fc229b113b9
joão <jantdantas@gmail.com>
2025-03-31 00:36:18 -0300

    feat: test user login

--- backend/tests/test_controllers/test_users.py
+++ backend/tests/test_controllers/test_users.py
@@ -1,10 +1,7 @@
 # Mexer aqui depois que adicionar o regex de email no back
 from models import User
-from routers import users
-from fastapi import FastAPI
-app = FastAPI()
+from dependencies.auth_dependency import hash_password
 
-app.include_router(users.router)
 
 def test_register_user_should_return_201(test_client, db_session):
     assert db_session.query(User).count() == 0
@@ -57,3 +54,42 @@ def test_register_user_should_return_400(test_client, db_session):
 
     response_data = response.json()
     assert response_data["detail"] == "A senha deve conter pelo menos 8 caracteres, uma letra maiúscula, uma letra minúscula e um caractere especial."
+
+
+def test_user_login_should_return_201(test_client, db_session):
+    assert db_session.query(User).count() == 0
+
+    hashed_password = hash_password("Senha123!")
+    db_session.add(User(name="teste", email="teste@gmail.com", password=hashed_password))
+    db_session.commit()
+
+    teste_case = {"email": "teste@gmail.com", "password": "Senha123!"}
+
+    response = test_client.post(
+        "/users/login", json=teste_case
+    )
+
+    response_data = response.json()
+    
+    assert response.status_code == 201
+
+    response_data = response.json()
+    assert "access_token" in response_data
+    assert response_data["token_type"] == "Bearer"
+
+
+def test_user_login_should_return_401(test_client, db_session):
+    assert db_session.query(User).count() == 0
+
+    teste_case = {"email": "teste@gmail.com", "password": "Senha123!"}
+
+    response = test_client.post(
+        "/users/login", json=teste_case
+    )
+
+    response_data = response.json()
+    
+    assert response.status_code == 401
+
+    response_data = response.json()
+    assert response_data["detail"] == "Incorrect username or password"
