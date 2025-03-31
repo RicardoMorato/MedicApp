458681414eb5cc69b33210ac646248a0d79ed970
joão <jantdantas@gmail.com>
2025-03-31 00:27:19 -0300

    feat: test register user should return 400

--- backend/tests/test_controllers/test_users.py
+++ backend/tests/test_controllers/test_users.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 app.include_router(users.router)
 
-def test_check_register_user_should_return_201(test_client, db_session):
+def test_register_user_should_return_201(test_client, db_session):
     assert db_session.query(User).count() == 0
 
     teste_case = {"name": "teste", "email": "teste@gmail.com", "password": "Senha123!"}
@@ -22,9 +22,9 @@ def test_check_register_user_should_return_201(test_client, db_session):
     assert response_data["token_type"] == "Bearer"
 
 
-def test_check_register_user_should_return_401(test_client, db_session):
+def test_register_user_should_return_401(test_client, db_session):
     assert db_session.query(User).count() == 0
-    
+
     db_session.add(User(name="teste", email="teste@gmail.com", password="Senha123!"))
     db_session.commit()
 
@@ -40,3 +40,20 @@ def test_check_register_user_should_return_401(test_client, db_session):
 
     response_data = response.json()
     assert response_data["detail"] == "User already created"
+
+
+def test_register_user_should_return_400(test_client, db_session):
+    assert db_session.query(User).count() == 0
+
+    teste_case = {"name": "teste", "email": "teste@gmail.com", "password": "Senha123"}
+
+    response = test_client.post(
+        "/users/signup", json=teste_case
+    )
+
+    response_data = response.json()
+    
+    assert response.status_code == 400
+
+    response_data = response.json()
+    assert response_data["detail"] == "A senha deve conter pelo menos 8 caracteres, uma letra maiúscula, uma letra minúscula e um caractere especial."
