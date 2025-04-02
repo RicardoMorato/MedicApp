ecadd3a414de7a33dcdda5aead6575be6e35b435
joão <jantdantas@gmail.com>
2025-04-01 21:40:54 -0300

    fix: changing names in endpoints

--- backend/routers/user_drugs.py
+++ backend/routers/user_drugs.py
@@ -10,11 +10,11 @@ from sqlalchemy import or_
 
 router = APIRouter(tags=["User Drugs"])
 
-@router.post("/users/{user_id}/drugs/add", status_code=status.HTTP_201_CREATED)
+@router.post("/users/{user_id}/drugs/", status_code=status.HTTP_201_CREATED)
 def add_drug(drug: DrugCreate, db: Session = Depends(get_db), current_user=Depends(get_current_user)):
     return controller.add_medicament_to_user(db, current_user, drug)
 
-@router.get("/user/{user_id}/drugs", status_code=status.HTTP_200_OK)
+@router.get("/user/{user_id}/medications/", status_code=status.HTTP_200_OK)
 async def search_medicamentos_route(db: Session = Depends(get_db), current_user=Depends(get_current_user), name: str = Query(None), skip: int = Query(0, ge=0), limit: int = Query(100, ge=1, le=17000)):
     user_id = current_user.id
     query = db.query(UserDrugs).filter(UserDrugs.user_id == user_id).order_by(UserDrugs.name)
