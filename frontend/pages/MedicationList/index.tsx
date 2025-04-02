b6d09ceb1b1732aafa0e5110d94f1facb86bff0e
eduardomatos7 <eduardo.matoss@ufpe.br>
2025-04-01 21:10:29 -0300

    fix: fixes medicaments being rendered and loading

--- frontend/pages/MedicationList/index.tsx
+++ frontend/pages/MedicationList/index.tsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState, useCallback } from 'react'
 import api from '../../services/api'
 import SplashLoading from '@/components/SplashLoading'
 import { Medication } from '@/interfaces/Medication'
-import { debounce } from 'lodash';
+import { debounce, set } from 'lodash';
 
 function MedicationList() {
   const [medications, setMedications] = useState<Medication[]>([]);
@@ -17,17 +17,19 @@ function MedicationList() {
 
   const fetchMedications = useCallback(
     debounce(() => {
-      setLoading(true);
-      api.get('/medicament/search/', {
-        params: {
-          skip: skip,
-          limit: limit,
-          name: searchQuery,
-        },
-      })
-      .then(response => {
-          const { items, total: totalCount } = response.data
-          setTotal(totalCount)
+      if (hasMore === false){
+      setMedications([])}
+      const params: any = {
+        skip: skip,
+        limit: limit,
+      };
+      if (searchQuery) {
+        params.name = searchQuery; 
+      }
+      api.get('/medicament/search/', { params })
+        .then(response => {
+          const { items, total: totalCount } = response.data;
+          setTotal(totalCount);
           const medicationsData = items.map((item: any) => ({
             id: item.id,
             medicamento: item.medicamento,
@@ -35,32 +37,39 @@ function MedicationList() {
             concentracao: item.concentracao,
             farmaco: item.farmaco,
           }));
-          
-          setMedications((prev) => {
+          setMedications(prev => {
             const existingIds = new Set(prev.map(med => med.id))
             const uniqueMedications = medicationsData.filter((med: Medication) => !existingIds.has(med.id))
-            return [...prev, ...uniqueMedications]
-          })
-          
+            return [...prev, ...uniqueMedications]; 
+          });
           setLoading(false);
+          
         })
         .catch(error => {
           console.error('Erro ao buscar medicamentos:', error);
           setLoading(false);
         });
-      }, 500), 
-      [skip, limit, searchQuery]
-    );
-    
-    console.log('Total de medicamentos:', total)
-    function handleEndReached() {
-      medications.length < total ? setHasMore(true) : setHasMore(false);
-      
-      if (hasMore && !loading) {
-        setSkip(prevSkip => prevSkip + 20);
-        setLimit(prevLimit => prevLimit + 20);
+    }, 500),
+    [skip, limit, searchQuery]
+  );
+
+  function handleEndReached() {
+    if (medications.length < total) {
+      setHasMore(true)
+      if (total < 300){ 
+        setLimit(total)
+      }else {
+        setLimit(prevLimit => prevLimit + 20)
       }
+    }else {
+      setHasMore(false)
+      return
     }
+  }
+  
+  console.log('Total de medicamentos:', total)
+  console.log(hasMore)
+  console.log('Medicamentos renderizados:', medications.length)
   useEffect(() => {
     fetchMedications();
 
@@ -74,16 +83,19 @@ function MedicationList() {
       setMedications([])
       fetchMedications()
       setSearchUsed(false)
+      setHasMore(true)
     }
   }, [ fetchMedications])
 
   const handleSearchQueryChange = (query: string) => {
+    setMedications([])
     setSearchQuery(query)
-    if (!searchUsed) setSearchUsed(true)
+    setSkip(0)
+    setLimit(20)
   }
 
   return (
-    (loading && medications.length === 0 
+    (loading
       ? <SplashLoading /> 
       : <MedicamentsListed 
         medications={medications} 
