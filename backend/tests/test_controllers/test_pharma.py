81d3c9c1f2d36ac64e9717a44070e4417c36414d
joão <jantdantas@gmail.com>
2025-04-03 01:41:29 -0300

    fix: error in test_pharma

--- backend/tests/test_controllers/test_pharma.py
+++ backend/tests/test_controllers/test_pharma.py
@@ -1,6 +1,6 @@
 from models import Pharma
 
-def test_list_pharma_with_different_parameters_should_retun_200(test_client, db_session):
+def test_list_pharma_with_a_valid_input_should_return_the_searched_pharma(test_client, db_session):
     assert db_session.query(Pharma).count() == 0
 
     pharma = Pharma(
@@ -18,6 +18,21 @@ def test_list_pharma_with_different_parameters_should_retun_200(test_client, db_
     assert response_filtered.status_code == 200
     assert len(response_filtered.json()) == 1
 
+
+def test_list_pharma_with_an_invalid_input_should_return_an_empty_list(test_client, db_session):
+    assert db_session.query(Pharma).count() == 0
+
+    pharma = Pharma(
+        pharma_name="Dipirona"
+    )
+
+    db_session.add(pharma)
+    db_session.commit()
+
+    response = test_client.get("/pharma")
+    assert response.status_code == 200
+    assert len(response.json()) == 1
+    
     response_filtered = test_client.get("/pharma?name=Novalgina")
     assert response_filtered.status_code == 200
     assert len(response_filtered.json()) == 0
