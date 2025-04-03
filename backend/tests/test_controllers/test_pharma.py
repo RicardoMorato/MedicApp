8e26434f31726e597e84cb608d2053a3c428c1b7
joão <jantdantas@gmail.com>
2025-04-03 03:52:29 -0300

    fix: adding more tests to the pharma router

--- backend/tests/test_controllers/test_pharma.py
+++ backend/tests/test_controllers/test_pharma.py
@@ -1,8 +1,6 @@
 from models import Pharma
 
-def test_list_pharma_with_a_valid_input_should_return_the_searched_pharma(test_client, db_session):
-    assert db_session.query(Pharma).count() == 0
-
+def create_pharma(db_session):
     pharma = Pharma(
         pharma_name="Dipirona"
     )
@@ -10,11 +8,37 @@ def test_list_pharma_with_a_valid_input_should_return_the_searched_pharma(test_c
     db_session.add(pharma)
     db_session.commit()
 
+    pharma2 = Pharma(
+        pharma_name="Aspirina"
+    )
+
+    db_session.add(pharma2)
+    db_session.commit()
+
+def test_list_pharma_with_none_input_should_return_all_pharmas(test_client, db_session):
+    assert db_session.query(Pharma).count() == 0
+
+    create_pharma(db_session)
     response = test_client.get("/pharma")
     assert response.status_code == 200
-    assert len(response.json()) == 1
+    assert len(response.json()) == 2
 
+def test_list_pharma_with_a_valid_input_should_return_the_searched_pharma(test_client, db_session):
+    assert db_session.query(Pharma).count() == 0
+
+    create_pharma(db_session)
+    
     response_filtered = test_client.get("/pharma?name=Dipirona")
+    assert response_filtered.status_code == 200
+    assert len(response_filtered.json()) == 1
+
+
+def test_list_pharma_with_a_half_input_should_return_the_half_searched_pharma(test_client, db_session):
+    assert db_session.query(Pharma).count() == 0
+
+    create_pharma(db_session)
+    
+    response_filtered = test_client.get("/pharma?name=dipi")
     assert response_filtered.status_code == 200
     assert len(response_filtered.json()) == 1
 
@@ -22,17 +46,8 @@ def test_list_pharma_with_a_valid_input_should_return_the_searched_pharma(test_c
 def test_list_pharma_with_an_invalid_input_should_return_an_empty_list(test_client, db_session):
     assert db_session.query(Pharma).count() == 0
 
-    pharma = Pharma(
-        pharma_name="Dipirona"
-    )
+    create_pharma(db_session)
 
-    db_session.add(pharma)
-    db_session.commit()
-
-    response = test_client.get("/pharma")
-    assert response.status_code == 200
-    assert len(response.json()) == 1
-    
-    response_filtered = test_client.get("/pharma?name=Novalgina")
+    response_filtered = test_client.get("/pharma?name=Zolpidem")
     assert response_filtered.status_code == 200
     assert len(response_filtered.json()) == 0
