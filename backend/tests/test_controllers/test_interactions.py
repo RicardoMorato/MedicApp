f5036d68b247d11b92905c6ae10cfa659f4b987e
joão <jantdantas@gmail.com>
2025-04-04 21:20:08 -0300

    fix: correcting some changes in previous tests logic

--- backend/tests/test_controllers/test_interactions.py
+++ backend/tests/test_controllers/test_interactions.py
@@ -1,5 +1,19 @@
 from models import Interaction # Certifique-se de importar o modelo correto
+import pytest
 
+@pytest.fixture
+def seed_interaction(db_session):
+    interaction = Interaction(
+        principio_ativo1="paracetamol",
+        principio_ativo2="dipirona",
+        gravidade_interacao="teste",
+        probabilidade_ocorrencia="teste",
+        efeito="teste",
+        inicio_interacao="teste",
+    )
+
+    db_session.add(interaction)
+    db_session.commit()
 
 def test_check_interactions_without_medicaments_stored_should_return_404(
     test_client, db_session
@@ -13,22 +27,7 @@ def test_check_interactions_without_medicaments_stored_should_return_404(
     assert response.json() == {"detail": "Esta interação não existe."}
 
 
-def test_check_interactions_with_a_match_should_return_200(test_client, db_session):
-    assert db_session.query(Interaction).count() == 0
-
-    # Armazena um medicamento no banco de dados
-    interaction = Interaction(
-        principio_ativo1="paracetamol",
-        principio_ativo2="dipirona",
-        gravidade_interacao="teste",
-        probabilidade_ocorrencia="teste",
-        efeito="teste",
-        inicio_interacao="teste",
-    )
-
-    db_session.add(interaction)
-    db_session.commit()
-
+def test_check_interactions_with_a_match_should_return_200(test_client, seed_interaction):
     response = test_client.post(
         "/interactions", json={"name_1": "paracetamol", "name_2": "dipirona"}
     )
@@ -37,6 +36,5 @@ def test_check_interactions_with_a_match_should_return_200(test_client, db_sessi
 
     assert response.status_code == 200
     assert data["message"] == "Interação medicamentosa existente"
-    assert data["interação"]["id"] == interaction.id
-    assert data["interação"]["principio_ativo1"] == interaction.principio_ativo1
-    assert data["interação"]["principio_ativo2"] == interaction.principio_ativo2
+    assert data["interação"]["principio_ativo1"] == "paracetamol"
+    assert data["interação"]["principio_ativo2"] == "dipirona"
--- backend/tests/test_controllers/test_pharma.py
+++ backend/tests/test_controllers/test_pharma.py
@@ -1,5 +1,7 @@
 from models import Pharma
+import pytest
 
+@pytest.fixture
 def create_pharma(db_session):
     pharma = Pharma(
         pharma_name="Dipirona"
@@ -15,39 +17,24 @@ def create_pharma(db_session):
     db_session.add(pharma2)
     db_session.commit()
 
-def test_list_pharma_with_none_input_should_return_all_pharmas(test_client, db_session):
-    assert db_session.query(Pharma).count() == 0
-
-    create_pharma(db_session)
+def test_list_pharma_with_none_input_should_return_all_pharmas(test_client, create_pharma):
     response = test_client.get("/pharma")
     assert response.status_code == 200
     assert len(response.json()) == 2
 
-def test_list_pharma_with_a_valid_input_should_return_the_searched_pharma(test_client, db_session):
-    assert db_session.query(Pharma).count() == 0
-
-    create_pharma(db_session)
-    
+def test_list_pharma_with_a_valid_input_should_return_the_searched_pharma(test_client, create_pharma):
     response_filtered = test_client.get("/pharma?name=Dipirona")
     assert response_filtered.status_code == 200
     assert len(response_filtered.json()) == 1
 
 
-def test_list_pharma_with_a_half_input_should_return_the_half_searched_pharma(test_client, db_session):
-    assert db_session.query(Pharma).count() == 0
-
-    create_pharma(db_session)
-    
+def test_list_pharma_with_a_half_input_should_return_the_half_searched_pharma(test_client, create_pharma):
     response_filtered = test_client.get("/pharma?name=dipi")
     assert response_filtered.status_code == 200
     assert len(response_filtered.json()) == 1
 
 
-def test_list_pharma_with_an_invalid_input_should_return_an_empty_list(test_client, db_session):
-    assert db_session.query(Pharma).count() == 0
-
-    create_pharma(db_session)
-
+def test_list_pharma_with_an_invalid_input_should_return_an_empty_list(test_client, create_pharma):
     response_filtered = test_client.get("/pharma?name=Zolpidem")
     assert response_filtered.status_code == 200
     assert len(response_filtered.json()) == 0
