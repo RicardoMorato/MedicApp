6ef536e66330d5074a3c533a666434262c2b4c4d
joão <jantdantas@gmail.com>
2025-04-05 20:00:20 -0300

    fix: excluding an useless router

--- backend/dependencies/auth_dependency.py
+++ backend/dependencies/auth_dependency.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta, timezone
 from typing import Annotated
-from fastapi import APIRouter, Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
 from pydantic import BaseModel, Field
 import jwt
@@ -31,22 +31,6 @@ class Token(BaseModel):
         example="Bearer"
     )
 
-
-@router.post("/token")
-async def login_for_access_token(
-    form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: Session = Depends(get_db) ) -> Token:
-    user = authenticate_user(db, form_data.username, form_data.password)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(
-        data={"sub": user.id}, expires_delta=access_token_expires
-    )
-    return Token(access_token=access_token, token_type="bearer")
 
 def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
     credentials_exception = HTTPException(
@@ -81,7 +65,7 @@ def create_access_token(data: dict, expires_delta: timedelta= None):
     if expires_delta:
         expire = datetime.now(timezone.utc) + expires_delta
     else:
-        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+        expire = datetime.now(timezone.utc) + timedelta(minutes=60)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.openapi.docs import get_redoc_html
-from dependencies.auth_dependency import router as auth
 from routers.users import router as users_router
 from routers.user_drugs import router as user_drugs
 from routers.medicament import router as pesquisar_medicamentos
@@ -20,7 +19,6 @@ app.add_middleware(
 
 app.include_router(users_router)
 app.include_router(user_drugs)
-app.include_router(auth)
 app.include_router(pesquisar_medicamentos)
 app.include_router(interactions)
 app.include_router(pharma)
