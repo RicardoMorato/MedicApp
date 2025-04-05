599f8d426f73111772c220a9f546bd3e61069718
Ricardo Morato <rmr@cin.ufpe.br>
2025-04-05 08:14:59 -0300

    chore: bind port 8000 instead of 80
    Signed-off-by: Ricardo Morato <rmr@cin.ufpe.br>

--- backend/start.sh
+++ backend/start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Inicia o servidor FastAPI em segundo plano
-uvicorn main:app --host 0.0.0.0 --port 80 &
+uvicorn main:app --host 0.0.0.0 --port 8000 &
 
 # Aguarda alguns segundos para garantir que o servidor esteja rodando
 sleep 5
