1d52676285a7f8769f41f8a41b013bb34be1066d
Ricardo Morato <rmr@cin.ufpe.br>
2025-04-06 10:17:55 -0300

    fix: capitalize header title
    Signed-off-by: Ricardo Morato <rmr@cin.ufpe.br>

--- frontend/pages/Profile/index.tsx
+++ frontend/pages/Profile/index.tsx
@@ -1,23 +1,22 @@
-import HeaderProfile from '@/components/HeaderProfile'
-import React from 'react'
-import { View } from 'react-native'
-import { styles } from './style'
-import FontLoader from '../../components/FontLoader';
-import HeaderTittle from '@/components/HeaderTittle'
-import PageContent from '@/components/AddedDrugs';
+import HeaderProfile from "@/components/HeaderProfile";
+import React from "react";
+import { View } from "react-native";
+import { styles } from "./style";
+import FontLoader from "../../components/FontLoader";
+import HeaderTittle from "@/components/HeaderTittle";
+import PageContent from "@/components/AddedDrugs";
 
 function Profile() {
-
   return (
     <>
       <FontLoader>
         <View style={styles.container}>
-          <HeaderTittle title="Meu perfil"/>
-          <PageContent/>
+          <HeaderTittle title="Meu Perfil" />
+          <PageContent />
         </View>
       </FontLoader>
     </>
-  )
+  );
 }
 
-export default Profile
+export default Profile;
