cc92c927fc390bb020e9843c7333db72a8af45e4
Ricardo Morato <rmr@cin.ufpe.br>
2025-04-06 10:18:53 -0300

    fix: add FontLoader to DrugInteractions
    Signed-off-by: Ricardo Morato <rmr@cin.ufpe.br>

--- frontend/pages/DrugInteraction/index.tsx
+++ frontend/pages/DrugInteraction/index.tsx
@@ -28,6 +28,7 @@ import {
 import { Dropdown } from "react-native-element-dropdown";
 import { Colors } from "@/constants/Colors";
 import HeaderTittle from "@/components/HeaderTittle";
+import FontLoader from "@/components/FontLoader";
 
 export default function DrugInteractionScreen() {
   const navigation = useNavigation<any>();
@@ -82,119 +83,128 @@ export default function DrugInteractionScreen() {
       behavior={Platform.OS === "ios" ? "padding" : "height"}
       keyboardVerticalOffset={Platform.OS == "ios" ? 0 : -150}
     >
-      <HeaderTittle title="Verificar Interações"/>
-      <Provider>
-      <View style={styles.container}>
-        
-          <View style={styles.content}>
-            {interactionResult != null && drugA !== null && drugB !== null && (
-              <InteractionResultPopover
-                drugA={drugA}
-                drugB={drugB}
-                result={interactionResult ?? true}
-                closeCallback={() => setInteractionResult(null)}
-              />
-            )}
-            <Text style={styles.subtitle}>
-              Verifique se você pode tomar dois medicamentos{" "}
-              <Text style={styles.highlight}>simultaneamente</Text>.
-            </Text>
-            <View style={styles.interactionContainer}>
-              <View style={styles.contentDropdown}>
-                <Dropdown
-                  style={[
-                    styles.dropdown,
-                    isFocusDrugA && { borderColor: Colors.light.primary, borderWidth: 1 },
-                  ]}
-                  placeholderStyle={styles.placeholderStyle}
-                  selectedTextStyle={styles.selectedTextStyle}
-                  inputSearchStyle={styles.inputSearchStyle}
-                  iconStyle={styles.iconStyle}
-                  data={parseDrugsToSelect(drugsList)}
-                  search
-                  autoScroll={false}
-                  maxHeight={300}
-                  labelField="value"
-                  valueField="value"
-                  placeholder={
-                    !isFocusDrugA ? "Selecione o primeiro medicamento" : "..."
-                  }
-                  searchPlaceholder="Pesquisar medicamento..."
-                  value={drugA}
-                  onFocus={() => setIsFocusDrugA(true)}
-                  onBlur={() => setIsFocusDrugA(false)}
-                  onChange={(item) => {
-                    setDrugA(item.value);
-                    setIsFocusDrugA(false)
+      <FontLoader>
+        <HeaderTittle title="Verificar Interações" />
+        <Provider>
+          <View style={styles.container}>
+            <View style={styles.content}>
+              {interactionResult != null &&
+                drugA !== null &&
+                drugB !== null && (
+                  <InteractionResultPopover
+                    drugA={drugA}
+                    drugB={drugB}
+                    result={interactionResult ?? true}
+                    closeCallback={() => setInteractionResult(null)}
+                  />
+                )}
+              <Text style={styles.subtitle}>
+                Verifique se você pode tomar dois medicamentos{" "}
+                <Text style={styles.highlight}>simultaneamente</Text>.
+              </Text>
+              <View style={styles.interactionContainer}>
+                <View style={styles.contentDropdown}>
+                  <Dropdown
+                    style={[
+                      styles.dropdown,
+                      isFocusDrugA && {
+                        borderColor: Colors.light.primary,
+                        borderWidth: 1,
+                      },
+                    ]}
+                    placeholderStyle={styles.placeholderStyle}
+                    selectedTextStyle={styles.selectedTextStyle}
+                    inputSearchStyle={styles.inputSearchStyle}
+                    iconStyle={styles.iconStyle}
+                    data={parseDrugsToSelect(drugsList)}
+                    search
+                    autoScroll={false}
+                    maxHeight={300}
+                    labelField="value"
+                    valueField="value"
+                    placeholder={
+                      !isFocusDrugA ? "Selecione o primeiro medicamento" : "..."
+                    }
+                    searchPlaceholder="Pesquisar medicamento..."
+                    value={drugA}
+                    onFocus={() => setIsFocusDrugA(true)}
+                    onBlur={() => setIsFocusDrugA(false)}
+                    onChange={(item) => {
+                      setDrugA(item.value);
+                      setIsFocusDrugA(false);
+                    }}
+                  />
+                </View>
+                <View
+                  style={{
+                    backgroundColor: "#fff",
+                    width: "100%",
+                    borderRadius: 10,
                   }}
-                />
+                >
+                  <Dropdown
+                    style={[
+                      styles.dropdown,
+                      isFocusDrugB && {
+                        borderColor: Colors.light.primary,
+                        borderWidth: 1,
+                      },
+                    ]}
+                    placeholderStyle={styles.placeholderStyle}
+                    selectedTextStyle={styles.selectedTextStyle}
+                    inputSearchStyle={styles.inputSearchStyle}
+                    iconStyle={styles.iconStyle}
+                    data={parseDrugsToSelect(drugsList)}
+                    search
+                    autoScroll={false}
+                    maxHeight={300}
+                    labelField="value"
+                    valueField="value"
+                    placeholder={
+                      !isFocusDrugB ? "Selecione o segundo medicamento" : "..."
+                    }
+                    searchPlaceholder="Pesquisar medicamento..."
+                    value={drugB}
+                    onFocus={() => setIsFocusDrugB(true)}
+                    onBlur={() => setIsFocusDrugB(false)}
+                    onChange={(item) => {
+                      setDrugB(item.value);
+                      setIsFocusDrugB(false);
+                    }}
+                  />
+                </View>
+
+                <View style={styles.medicamentsImageContainer}>
+                  <Image
+                    source={MedicationImage}
+                    resizeMode="contain"
+                    style={{ height: 60, borderRadius: 50, width: 60 }}
+                  ></Image>
+                </View>
               </View>
-              <View
-                style={{
-                  backgroundColor: "#fff",
-                  width: "100%",
-                  borderRadius: 10,
-                }}
+
+              <TouchableOpacity
+                onPress={checkDrugsInteraction}
+                style={styles.button}
               >
-                <Dropdown
-                  style={[
-                    styles.dropdown,
-                    isFocusDrugB && { borderColor: Colors.light.primary, borderWidth: 1 },
-                  ]}
-                  placeholderStyle={styles.placeholderStyle}
-                  selectedTextStyle={styles.selectedTextStyle}
-                  inputSearchStyle={styles.inputSearchStyle}
-                  iconStyle={styles.iconStyle}
-                  data={parseDrugsToSelect(drugsList)}
-                  search
-                  autoScroll={false}
-                  maxHeight={300}
-                  labelField="value"
-                  valueField="value"
-                  placeholder={
-                    !isFocusDrugB ? "Selecione o segundo medicamento" : "..."
-                  }
-                  searchPlaceholder="Pesquisar medicamento..."
-                  value={drugB}
-                  onFocus={() => setIsFocusDrugB(true)}
-                  onBlur={() => setIsFocusDrugB(false)}
-                  onChange={(item) => {
-                    setDrugB(item.value);
-                    setIsFocusDrugB(false)
-                  }}
-                />
-              </View>
-
-              <View style={styles.medicamentsImageContainer}>
-                <Image
-                  source={MedicationImage}
-                  resizeMode="contain"
-                  style={{ height: 60, borderRadius: 50, width: 60 }}
-                ></Image>
-              </View>
+                <View style={styles.sectionButton}>
+                  <Text style={styles.buttonText}>Verificar Interação</Text>
+                  <Image
+                    source={arrowImage}
+                    resizeMode="contain"
+                    style={{ height: 32, width: 32 }}
+                  ></Image>
+                </View>
+              </TouchableOpacity>
+              <Text style={styles.disclaimer}>
+                Este aplicativo avalia interações medicamentosas, mas não
+                substitui a consulta médica. Sempre busque orientação de um
+                profissional de saúde.
+              </Text>
             </View>
-
-            <TouchableOpacity
-              onPress={checkDrugsInteraction}
-              style={styles.button}
-            >
-              <View style={styles.sectionButton}>
-                <Text style={styles.buttonText}>Verificar Interação</Text>
-                <Image
-                  source={arrowImage}
-                  resizeMode="contain"
-                  style={{ height: 32, width: 32 }}
-                ></Image>
-              </View>
-            </TouchableOpacity>
-            <Text style={styles.disclaimer}>
-              Este aplicativo avalia interações medicamentosas, mas não
-              substitui a consulta médica. Sempre busque orientação de um
-              profissional de saúde.
-            </Text>
           </View>
-        </View>
-      </Provider>
+        </Provider>
+      </FontLoader>
     </KeyboardAvoidingView>
   );
 }
