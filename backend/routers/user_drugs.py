4e3e73df9e8a13b23995b996e50d5bb067c4b7bb
Ricardo Morato <rmr@cin.ufpe.br>
2025-04-06 19:24:57 -0300

    fix: user drugs listing
    Signed-off-by: Ricardo Morato <rmr@cin.ufpe.br>

--- backend/routers/user_drugs.py
+++ backend/routers/user_drugs.py
@@ -1,49 +1,83 @@
 from fastapi import APIRouter, Depends, status, Query, Path
 from sqlalchemy.orm import Session
-from controllers import user_drugs as controller  
-from database import get_db
-from schemas.user_drugs import DrugCreate, DrugCreateResponse, DrugResponse
-from docs.user_drugs import response_user_drug_list, response_user_drug_create, response_user_drug_delete, description_user_drug_create, description_user_drug_delete, description_user_drug_list
-from dependencies.auth_dependency import get_current_user
 from typing import List
 
+from controllers import user_drugs as controller
+from database import get_db
+from schemas.user_drugs import DrugCreate, DrugCreateResponse, DrugResponse
+from docs.user_drugs import (
+    response_user_drug_list,
+    response_user_drug_create,
+    response_user_drug_delete,
+    description_user_drug_create,
+    description_user_drug_delete,
+    description_user_drug_list,
+)
+from dependencies.auth_dependency import get_current_user
+
+
 router = APIRouter(tags=["User Drugs"])
+
 
 @router.post(
     "/users/{user_id}/drugs/",
     summary="Adicionar medicamento do usuário",
     status_code=status.HTTP_201_CREATED,
-    response_model= DrugCreateResponse,
+    response_model=DrugCreateResponse,
     responses=response_user_drug_create,
-    description=description_user_drug_create
+    description=description_user_drug_create,
 )
-def add_drug(drug: DrugCreate, db: Session = Depends(get_db), current_user=Depends(get_current_user)):
+def add_drug(
+    drug: DrugCreate,
+    db: Session = Depends(get_db),
+    current_user=Depends(get_current_user),
+):
     return controller.add_medicament_to_user(db, current_user, drug)
 
 
+@router.delete(
+    "/users/{user_id}/drugs/{drug_id}",
+    summary="Deletar medicamentos do usuário",
+    status_code=status.HTTP_204_NO_CONTENT,
+    responses=response_user_drug_delete,
+    description=description_user_drug_delete,
+)
+def delete_drug(
+    drug_id: int = Path(
+        ...,
+        title="ID do medicamento do usuário",
+        description="ID no banco de dados do medicamento cadastrado pelo usuário",
+        example=10,
+    ),
+    db: Session = Depends(get_db),
+    current_user=Depends(get_current_user),
+):
+    return controller.delete_user_drug(db, current_user, drug_id)
+
+
 @router.get(
-    "/user/{user_id}/medications/",
+    "/user/{user_id}/medications",
     summary="Listar medicamentos do usuário",
     status_code=status.HTTP_200_OK,
     response_model=List[DrugResponse],
     responses=response_user_drug_list,
-    description=description_user_drug_list
+    description=description_user_drug_list,
 )
-async def search_user_medications_route(
+async def search_user_medications(
+    user=Depends(get_current_user),
     db: Session = Depends(get_db),
-    current_user=Depends(get_current_user),
     name: str = Query(
         None,
         title="Nome do Medicamento",
         description="Nome (ou parte do nome) do medicamento a ser buscado.",
-        example="Ibuprofeno"
+        example="Ibuprofeno",
     ),
     skip: int = Query(
         0,
         ge=0,
         title="Pular registros",
         description="Número de registros a serem ignorados (útil para paginação).",
-        example=1
+        example=1,
     ),
     limit: int = Query(
         100,
@@ -51,22 +85,7 @@ async def search_user_medications_route(
         le=17000,
         title="Limite de resultados",
         description="Número máximo de medicamentos a serem retornados.",
-        example=20
-    )
+        example=20,
+    ),
 ):
-    return controller.search_medicament_user(db, current_user, name, skip, limit)
-
-@router.delete(
-    "/users/{user_id}/drugs/{drug_id}",
-    summary="Deletar medicamentos do usuário",
-    status_code=status.HTTP_204_NO_CONTENT,
-    responses=response_user_drug_delete,
-    description=description_user_drug_delete
-)
-def delete_drug(drug_id: int = Path(
-        ...,
-        title="ID do medicamento do usuário",
-        description="ID no banco de dados do medicamento cadastrado pelo usuário",
-        example=10), 
-        db: Session = Depends(get_db), current_user=Depends(get_current_user)):
-    return controller.delete_user_drug(db, current_user, drug_id)
+    return controller.search_medicament_user(db, user, name, skip, limit)
--- frontend/components/AddedDrugs.tsx
+++ frontend/components/AddedDrugs.tsx
@@ -1,37 +1,48 @@
 import React, { useEffect, useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
-import { MedicamentsListedUser } from "@/components/MedicamentsListUser";
+import { AxiosError } from "axios";
 import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import { useIsFocused } from "@react-navigation/native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+import { MedicamentsListedUser } from "@/components/MedicamentsListUser";
 import { Colors } from "@/constants/Colors";
 import { MedicationUser } from "@/interfaces/Medication";
 import api from "@/services/api";
 import { utilDecodeToken } from "@/utils/utilDecodeToken";
-import AsyncStorage from "@react-native-async-storage/async-storage";
 import HeaderProfile from "@/components/HeaderProfile";
 
 function PageContent() {
+  const isFocused = useIsFocused();
+
   const [medications, setMedications] = useState<MedicationUser[]>([]);
   const [amount, setAmount] = useState<number>(0);
+
   async function fetchMedications() {
     const userId = await utilDecodeToken();
     const token = (await AsyncStorage.getItem("userToken")) || "";
-    api
-      .get(`/user/${userId}/medications`, {
+
+    try {
+      const response = await api.get(`/user/${userId}/medications`, {
         headers: {
           Authorization: `Bearer ${token}`,
         },
-      })
-      .then((Response) => {
-        setMedications(Response.data);
-        setAmount(Response.data.length);
-      })
-      .catch((error) => {
-        console.log(error);
       });
+
+      setMedications(response.data);
+      setAmount(response.data.length);
+    } catch (err: any) {
+      const error = err as AxiosError;
+
+      console.error(
+        "Erro ao buscar medicamentos:",
+        error.response?.data || error
+      );
+    }
   }
   useEffect(() => {
     fetchMedications();
-  }, []);
+  }, [isFocused]);
 
   return (
     <>
