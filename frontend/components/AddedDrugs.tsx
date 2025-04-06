af19d7ef6ec72ec967de5d5303c62565fe0a3400
Ricardo Morato <rmr@cin.ufpe.br>
2025-04-06 11:33:58 -0300

    fix: 'amount' spelling
    Signed-off-by: Ricardo Morato <rmr@cin.ufpe.br>

--- frontend/components/AddedDrugs.tsx
+++ frontend/components/AddedDrugs.tsx
@@ -10,62 +10,46 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import HeaderProfile from '@/components/HeaderProfile';
 
 function PageContent() {
-    const [medications, setMedications] = useState<MedicationUser[]>([]);
-    const [ammount, setAmount] = useState<number>(0);
-    async function fetchMedications() {
-        const userId = await utilDecodeToken()
-        const token = await AsyncStorage.getItem("userToken") || ""
-        api.get(`/user/${userId}/medications`, {
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        }).then((Response) => {
-            setMedications(Response.data)
-            setAmount(Response.data.length)
-        }).catch((error) => {
-            console.log(error)
-        }
-        )
-    }
-    useEffect(() => {
-        fetchMedications()
-    }, [])
-    
-    return (
-        <>
-    <HeaderProfile ammount={ammount} />
-    <View style={styles.contentTitle}>
-        <MaterialCommunityIcons name="pill" size={28} color={Colors.light.primary} />
+
+  return (
+    <>
+      <HeaderProfile amount={amount} />
+      <View style={styles.contentTitle}>
+        <MaterialCommunityIcons
+          name="pill"
+          size={28}
+          color={Colors.light.primary}
+        />
         <Text style={styles.title}>Meus medicamentos adicionados</Text>
-    </View>
-    <View style={styles.content}>
-          <MedicamentsListedUser medications={medications} />
-    </View>
+      </View>
+      <View style={styles.content}>
+        <MedicamentsListedUser medications={medications} />
+      </View>
     </>
-    )
+  );
 }
 
-export default PageContent
+export default PageContent;
 
 const styles = StyleSheet.create({
-    content: {
-        height: 400,
-        paddingVertical: 20,
-        paddingHorizontal: 5,
-        backgroundColor: Colors.light.backgroundBoxProfile,
-        marginHorizontal: 20,
-        borderRadius: 20,
-    },
-    contentTitle: {
-        flexDirection: 'row',
-        gap: 5,
-        marginTop: 30,
-        marginBottom: 15,
-        marginHorizontal: 20,
-    },
-    title: {
-        fontFamily: 'Poppins_600SemiBold',
-        fontSize: 19,
-        color: Colors.light.text,
-    }
-})
+  content: {
+    height: 400,
+    paddingVertical: 20,
+    paddingHorizontal: 5,
+    backgroundColor: Colors.light.backgroundBoxProfile,
+    marginHorizontal: 20,
+    borderRadius: 20,
+  },
+  contentTitle: {
+    flexDirection: "row",
+    gap: 5,
+    marginTop: 30,
+    marginBottom: 15,
+    marginHorizontal: 20,
+  },
+  title: {
+    fontFamily: "Poppins_600SemiBold",
+    fontSize: 19,
+    color: Colors.light.text,
+  },
+});
--- frontend/components/HeaderProfile.tsx
+++ frontend/components/HeaderProfile.tsx
@@ -1,99 +1,121 @@
-import React, { useState } from 'react'
-import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import { Colors } from '@/constants/Colors'
-import userIcon from '@/assets/icons/userIcon2.png'
-import MaterialIcons from '@expo/vector-icons/MaterialIcons'; 
-import { useNavigation } from 'expo-router';
-import { logOut } from '@/services/auth';
-import { fetchUserEmail, fetchusername } from '@/utils/fetchUserData';
+import React, { useState } from "react";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Colors } from "@/constants/Colors";
+import userIcon from "@/assets/icons/userIcon2.png";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import { useNavigation } from "expo-router";
+import { logOut } from "@/services/auth";
+import { fetchUserEmail, fetchusername } from "@/utils/fetchUserData";
 
-function HeaderProfile({ ammount }: { ammount: number }) {
-    const navigation = useNavigation<any>();
-    const [username, setUsername] = useState<string>("");
-    const [userEmail, setUserEmail] = useState<string>("");
-    fetchusername().then((user) => {
-        if (user) {
-            setUsername(user);  
-        }})
-    fetchUserEmail().then((email) => {
-        if (email) {
-            setUserEmail(email);
-        }})
-    
+function HeaderProfile({ amount }: { amount: number }) {
+  const navigation = useNavigation<any>();
+  const [username, setUsername] = useState<string>("");
+  const [userEmail, setUserEmail] = useState<string>("");
+  fetchusername().then((user) => {
+    if (user) {
+      setUsername(user);
+    }
+  });
+  fetchUserEmail().then((email) => {
+    if (email) {
+      setUserEmail(email);
+    }
+  });
 
-    return (
-
-            <View style={styles.content}>
-                <View style={styles.headerContent}>
-                    <View style={styles.imageUser}>
-                        <Image source={userIcon} resizeMode="contain" style={{height: 50}}/>
-                    </View>
-                    <View style={{ flexDirection: 'column', justifyContent: 'center', alignItems: 'flex-start' }}>
-                        <Text style={{ fontSize: 15, fontFamily: 'Poppins_500Medium'}}>{username}</Text>
-                        <Text style={{ fontSize: 12, color: '#555', fontFamily: 'Poppins_200ExtraLight' }}>{userEmail}</Text>
-                    </View>
-                </View>
-                <TouchableOpacity style={styles.exitContent} onPress={() => logOut(navigation)}>
-                    <MaterialIcons name="exit-to-app" size={24} color="red" />
-                    <Text style={styles.exitText}>Sair</Text>
-                </TouchableOpacity>
-                <View style={styles.dividerCard}/>
-                <View style={styles.countMedicaments}>
-                    <Text style={{ fontSize: 16, fontWeight: 'bold' }}>{ammount}</Text>
-                    <Text style={{ fontSize: 16, color: '#555' }}>Medicamentos</Text>
-                </View>
-            </View>
-
-    );
+  return (
+    <View style={styles.content}>
+      <View style={styles.headerContent}>
+        <View style={styles.imageUser}>
+          <Image
+            source={userIcon}
+            resizeMode="contain"
+            style={{ height: 50 }}
+          />
+        </View>
+        <View
+          style={{
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "flex-start",
+          }}
+        >
+          <Text style={{ fontSize: 15, fontFamily: "Poppins_500Medium" }}>
+            {username}
+          </Text>
+          <Text
+            style={{
+              fontSize: 12,
+              color: "#555",
+              fontFamily: "Poppins_200ExtraLight",
+            }}
+          >
+            {userEmail}
+          </Text>
+        </View>
+      </View>
+      <TouchableOpacity
+        style={styles.exitContent}
+        onPress={() => logOut(navigation)}
+      >
+        <MaterialIcons name="exit-to-app" size={24} color="red" />
+        <Text style={styles.exitText}>Sair</Text>
+      </TouchableOpacity>
+      <View style={styles.dividerCard} />
+      <View style={styles.countMedicaments}>
+        <Text style={{ fontSize: 16, fontWeight: "bold" }}>{amount}</Text>
+        <Text style={{ fontSize: 16, color: "#555" }}>Medicamentos</Text>
+      </View>
+    </View>
+  );
 }
 
-export default HeaderProfile
+export default HeaderProfile;
 const styles = StyleSheet.create({
-    content: {
-        paddingVertical: 20,
-        paddingHorizontal: 20,
-        backgroundColor: Colors.light.backgroundBoxProfile,
-        marginHorizontal: 20,
-        borderRadius: 20,
-        marginTop: 20,
-    },
-    headerContent: {
-        flexDirection: 'row',
-        alignItems: 'center',
-    },
-    imageUser: {
-        width: 70,
-        height: 70,
-        borderRadius: 999,
-        backgroundColor: Colors.light.background,
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginRight: 10
-    },
-    exitContent: {
-        position: 'absolute',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        top: 17,
-        right: 20,
-    },
-    exitText: {
-        fontSize: 12,
-        color: Colors.text.primary,
-        fontFamily: 'Poppins_500Medium',
-        marginTop: 5
-    },
-    dividerCard: {
-        width: '100%',
-        height: 1,
-        backgroundColor: Colors.light.separatorCard,
-        marginTop: 15,
-      },
-      countMedicaments: {
-        marginTop: 20,
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }
-})
+  content: {
+    paddingVertical: 20,
+    paddingHorizontal: 20,
+    backgroundColor: Colors.light.backgroundBoxProfile,
+    marginHorizontal: 20,
+    borderRadius: 20,
+    marginTop: 20,
+  },
+  headerContent: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  imageUser: {
+    width: 70,
+    height: 70,
+    borderRadius: 999,
+    backgroundColor: Colors.light.background,
+    justifyContent: "center",
+    alignItems: "center",
+    marginRight: 10,
+  },
+  exitContent: {
+    position: "absolute",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    top: 17,
+    right: 20,
+  },
+  exitText: {
+    fontSize: 12,
+    color: Colors.text.primary,
+    fontFamily: "Poppins_500Medium",
+    marginTop: 5,
+  },
+  dividerCard: {
+    width: "100%",
+    height: 1,
+    backgroundColor: Colors.light.separatorCard,
+    marginTop: 15,
+  },
+  countMedicaments: {
+    marginTop: 20,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
